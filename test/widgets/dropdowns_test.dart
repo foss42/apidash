ef2035252f7ee8202363b6d2bc3a5effa6598c15
Ashita Prasad <ashitaprasad92@gmail.com>
2023-04-22 03:50:58 +0530

    Added tests for DropdownButtonContentType

--- test/widgets/dropdowns_test.dart
+++ test/widgets/dropdowns_test.dart
@@ -45,4 +45,46 @@ void main() {
 
     expect(changedValue, HTTPVerb.put);
   });
+
+  testWidgets('Testing Dropdown for Content Type', (tester) async {
+    dynamic changedValue;
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Dropdown Content Type testing',
+        theme: kThemeDataLight,
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                DropdownButtonContentType(
+                  contentType: ContentType.json,
+                  onChanged: (value) {
+                    changedValue = value!;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byIcon(Icons.unfold_more_rounded), findsOneWidget);
+    expect(find.byType(DropdownButton<ContentType>), findsOneWidget);
+    expect(
+        (tester.widget(find.byType(DropdownButton<ContentType>))
+                as DropdownButton)
+            .value,
+        equals(ContentType.json));
+
+    await tester.tap(find.text('json'));
+    await tester.pump();
+    await tester.pump(const Duration(seconds: 1));
+
+    await tester.tap(find.text('text').last);
+    await tester.pump();
+    await tester.pump(const Duration(seconds: 1));
+
+    expect(changedValue, ContentType.text);
+  });
 }
