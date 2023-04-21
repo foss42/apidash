cc6fedaebee5b86e0654d8f2519dcdaa47ec4727
Ashita Prasad <ashitaprasad92@gmail.com>
2023-04-22 03:45:14 +0530

    Create dropdowns_test.dart

---
+++ test/widgets/dropdowns_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:apidash/widgets/dropdowns.dart';
+import 'package:apidash/consts.dart';
+import '../test_consts.dart';
+
+void main() {
+  testWidgets('Testing Dropdowns', (tester) async {
+    dynamic changedValue;
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Dropdown testing',
+        theme: kThemeDataLight,
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                DropdownButtonHttpMethod(
+                  method: HTTPVerb.post,
+                  onChanged: (value) {
+                    changedValue = value!;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byIcon(Icons.unfold_more_rounded), findsOneWidget);
+    expect(find.byType(DropdownButton<HTTPVerb>), findsOneWidget);
+    expect(
+        (tester.widget(find.byType(DropdownButton<HTTPVerb>)) as DropdownButton)
+            .value,
+        equals(HTTPVerb.post));
+
+    await tester.tap(find.text('POST'));
+    await tester.pump();
+    await tester.pump(const Duration(seconds: 1));
+
+    await tester.tap(find.text('PUT').last);
+    await tester.pump();
+    await tester.pump(const Duration(seconds: 1));
+
+    expect(changedValue, HTTPVerb.put);
+  });
+}
