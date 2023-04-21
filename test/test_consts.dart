740e629f8bbce88a2c3b17a170fb9702720c2f12
Ashita Prasad <ashitaprasad92@gmail.com>
2023-04-22 00:11:15 +0530

    Create test_consts.dart

---
+++ test/test_consts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final kThemeDataDark = ThemeData(
+  useMaterial3: true,
+  colorSchemeSeed: Colors.blue,
+  brightness: Brightness.dark,
+);
+
+final kThemeDataLight = ThemeData(
+  useMaterial3: true,
+  colorSchemeSeed: Colors.blue,
+  brightness: Brightness.light,
+);
