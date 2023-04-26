ac9fd8415e0e9e01fa98fca67caaccccb67cdf84
Ankit Mahato <ankmahato@gmail.com>
2023-04-26 16:17:58 +0530

    Update hive_services.dart

--- lib/services/hive_services.dart
+++ lib/services/hive_services.dart
@@ -1,16 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 const String kDataBox = "data";
 const String kKeyDataBoxIds = "ids";
 
 const String kSettingsBox = "settings";
-const String kKeySettingsBoxDarkMode = "darkMode";
 
 Future<void> openBoxes() async {
   await Hive.initFlutter();
   await Hive.openBox(kDataBox);
   await Hive.openBox(kSettingsBox);
 }
+
+Size? getInitialSize() {
+  var settingsBox = Hive.box(kSettingsBox);
+  double? w = settingsBox.get("width") as double?;
+  double? h = settingsBox.get("height") as double?;
+  if (w != null && h != null) {
+    return Size(w, h);
+  }
+  return null;
+}
+
+final hiveHandler = HiveHandler();
 
 class HiveHandler {
   late final Box dataBox;
@@ -21,9 +33,8 @@ class HiveHandler {
     settingsBox = Hive.box(kSettingsBox);
   }
 
-  dynamic getDarkMode() => settingsBox.get(kKeySettingsBoxDarkMode);
-  Future<void> setDarkMode(bool isDark) =>
-      settingsBox.put(kKeySettingsBoxDarkMode, isDark);
+  Map get settings => settingsBox.toMap();
+  Future<void> saveSettings(Map data) => settingsBox.putAll(data);
 
   dynamic getIds() => dataBox.get(kKeyDataBoxIds);
   Future<void> setIds(List<String>? ids) => dataBox.put(kKeyDataBoxIds, ids);
