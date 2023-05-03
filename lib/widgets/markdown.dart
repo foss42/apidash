de40ddd79610aec56101bf4681a3333b806fbdae
Ankit Mahato <ankmahato@gmail.com>
2023-05-03 22:30:41 +0530

    Markdown Widgets

---
+++ lib/widgets/markdown.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
+import 'package:url_launcher/url_launcher.dart';
+import 'buttons.dart';
+
+class CustomMarkdown extends StatefulWidget {
+  const CustomMarkdown({
+    super.key,
+    required this.data,
+  });
+  final String data;
+  @override
+  State<CustomMarkdown> createState() => _CustomMarkdownState();
+}
+
+class _CustomMarkdownState extends State<CustomMarkdown> {
+  @override
+  Widget build(BuildContext context) {
+    final mdStyleSheet = MarkdownStyleSheet(
+      h1: Theme.of(context).textTheme.headlineLarge,
+      p: Theme.of(context).textTheme.titleMedium,
+    );
+    return Markdown(
+      styleSheet: mdStyleSheet,
+      padding: const EdgeInsets.symmetric(
+        vertical: 40,
+        horizontal: 60,
+      ),
+      data: widget.data,
+      selectable: true,
+      extensionSet: md.ExtensionSet.gitHubFlavored,
+      onTapLink: (text, href, title) {
+        launchUrl(Uri.parse(href ?? ""));
+      },
+      builders: {
+        "inlineButton": InlineButton(),
+      },
+      inlineSyntaxes: [
+        InlineButtonSyntax(),
+      ],
+      blockSyntaxes: const [
+        SpacerSyntax(),
+      ],
+    );
+  }
+}
+
+class InlineButtonSyntax extends md.InlineSyntax {
+  InlineButtonSyntax({
+    String pattern = r'~`(.*?)`~',
+  }) : super(pattern);
+
+  @override
+  bool onMatch(md.InlineParser parser, Match match) {
+    final withoutDashes = match.group(0)!.replaceAll(RegExp(r'[~`]'), "");
+
+    md.Element el = md.Element.text("inlineButton", withoutDashes);
+
+    parser.addNode(el);
+    return true;
+  }
+}
+
+class InlineButton extends MarkdownElementBuilder {
+  @override
+  Widget visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    var txt = element.textContent;
+    switch (txt.toLowerCase()) {
+      case "star on github":
+        return SizedBox(
+          height: 24,
+          child: RepoButton(
+            text: txt,
+            icon: Icons.star,
+          ),
+        );
+      case "github repo":
+        return SizedBox(
+          height: 24,
+          child: RepoButton(
+            text: txt,
+            icon: Icons.code_rounded,
+          ),
+        );
+      case "discord server":
+        return SizedBox(
+          height: 24,
+          child: DiscordButton(
+            text: txt,
+          ),
+        );
+      default:
+        return const SizedBox();
+    }
+  }
+}
+
+final _spacerPattern = RegExp(r'^#br[ \x09\x0b\x0c]*$');
+
+class SpacerSyntax extends md.BlockSyntax {
+  @override
+  RegExp get pattern => _spacerPattern;
+
+  const SpacerSyntax();
+
+  @override
+  md.Node parse(md.BlockParser parser) {
+    pattern.firstMatch(parser.current.content)!;
+    parser.advance();
+    return md.Element('p', [md.Element.empty('p')]);
+  }
+}
