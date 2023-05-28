8e9c2046abda9af107647a7b65ad68a83aeb0db3
mmjsmohit <mmjsmohit@gmail.com>
2023-05-28 18:45:05 +0530

    Language Dropdown added.

--- lib/widgets/codegen_previewer.dart
+++ lib/widgets/codegen_previewer.dart
@@ -1,7 +1,10 @@
+import 'package:apidash/widgets/dropdowns.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:highlighter/highlighter.dart' show highlight;
 import 'package:apidash/consts.dart';
 import 'package:apidash/utils/utils.dart';
+import '../providers/ui_providers.dart';
 import 'code_previewer.dart' show convert;
 import 'buttons.dart';
 
@@ -144,6 +147,7 @@ class _ViewCodePaneState extends State<ViewCodePane> {
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                 ),
+                const DropdownButtonCodeCodegenLanguage(),
                 CopyButton(toCopy: widget.code),
                 SaveInDownloadsButton(
                   content: stringToBytes(widget.code),
@@ -168,6 +172,29 @@ class _ViewCodePaneState extends State<ViewCodePane> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class DropdownButtonCodeCodegenLanguage extends ConsumerStatefulWidget {
+  const DropdownButtonCodeCodegenLanguage({
+    super.key,
+  });
+
+  @override
+  ConsumerState createState() => _DropdownButtonCodeCodegenLanguageState();
+}
+
+class _DropdownButtonCodeCodegenLanguageState
+    extends ConsumerState<DropdownButtonCodeCodegenLanguage> {
+  @override
+  Widget build(BuildContext context) {
+    final requestCodeLanguage = ref.watch(codegenLanguageStateProvider);
+    return DropdownButtonCodegenLanguage(
+      codegenLanguage: requestCodeLanguage,
+      onChanged: (CodegenLanguage? value) {
+        ref.read(codegenLanguageStateProvider.notifier).state = value!;
+      },
     );
   }
 }
