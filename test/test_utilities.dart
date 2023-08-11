8c0ad975546cc9298d53ea66c3940fb70e2a0b4d
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2023-08-11 19:28:59 +0530

    Update test_utilities.dart
    Encapsulate into a Class: You could encapsulate the random string generation logic into a class for better organization and reusability.
    dart

--- test/test_utilities.dart
+++ test/test_utilities.dart
@@ -1,15 +1,17 @@
 import 'dart:math';
 
-const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-Random _rnd = Random();
+class RandomStringGenerator {
+  static const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  static Random _rnd = Random();
 
-String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+  static String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 
-String getRandomStringLines(int lines, int length) {
-  List<String> result = [];
-  for (var i = 0; i < lines; i++) {
-    result.add(getRandomString(length));
+  static String getRandomStringLines(int lines, int length) {
+    List<String> result = [];
+    for (var i = 0; i < lines; i++) {
+      result.add(getRandomString(length));
+    }
+    return result.join('\n');
   }
-  return result.join('\n');
 }
