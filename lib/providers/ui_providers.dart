adb14c22c7c6c1176bcc6c25d8af99ffb0abf1ec
Ashita Prasad <ashitaprasad92@gmail.com>
2023-10-08 02:39:34 +0530

    Update Dashboard NavigationRail

--- lib/providers/ui_providers.dart
+++ lib/providers/ui_providers.dart
@@ -1,11 +1,12 @@
 import 'package:apidash/consts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final navRailIndexStateProvider = StateProvider<int?>((ref) => 0);
+final navRailIndexStateProvider = StateProvider<int>((ref) => 0);
 final activeIdStateProvider = StateProvider<String?>((ref) => null);
 final activeIdEditStateProvider = StateProvider<String?>((ref) => null);
 final sentRequestIdStateProvider = StateProvider<String?>((ref) => null);
 final codePaneVisibleStateProvider = StateProvider<bool>((ref) => false);
 final saveDataStateProvider = StateProvider<bool>((ref) => false);
 final clearDataStateProvider = StateProvider<bool>((ref) => false);
-final codegenLanguageStateProvider = StateProvider<CodegenLanguage>((ref) => CodegenLanguage.dartHttp);
+final codegenLanguageStateProvider =
+    StateProvider<CodegenLanguage>((ref) => CodegenLanguage.dartHttp);
