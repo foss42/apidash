3c619eef951a9446a18572f1f3d53f0bed689f05
Ashita Prasad <ashitaprasad92@gmail.com>
2023-11-11 23:31:13 +0530

    Add focus provider

--- lib/providers/ui_providers.dart
+++ lib/providers/ui_providers.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final navRailIndexStateProvider = StateProvider<int>((ref) => 0);
@@ -6,3 +7,20 @@ final sentRequestIdStateProvider = StateProvider<String?>((ref) => null);
 final codePaneVisibleStateProvider = StateProvider<bool>((ref) => false);
 final saveDataStateProvider = StateProvider<bool>((ref) => false);
 final clearDataStateProvider = StateProvider<bool>((ref) => false);
+// final nameTextFieldControllerProvider =
+//     StateProvider.autoDispose<TextEditingController>((ref) {
+//   TextEditingController controller = TextEditingController(text: "");
+//   ref.onDispose(() {
+//     controller.dispose();
+//   });
+//   return controller;
+// });
+
+final nameTextFieldFocusNodeProvider =
+    StateProvider.autoDispose<FocusNode>((ref) {
+  FocusNode focusNode = FocusNode();
+  ref.onDispose(() {
+    focusNode.dispose();
+  });
+  return focusNode;
+});
