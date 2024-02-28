41784f35c49fb987faa11ed400fcf29736dbbaf7
Sebastian Büchler <sebastian.buechler@kasparund.ch>
2024-02-28 17:50:36 +0100

    remove unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,2 @@
-import 'package:apidash/main.dart';
-import 'package:apidash/app.dart';
-import 'package:apidash/common/utils.dart';
-import 'package:apidash/screens/screens.dart';
 
 void main() {}
