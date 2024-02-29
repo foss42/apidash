9c6bb5ae21407cb75f37715f25dd5b37769dacd6
DILPREET1910 <dsahney1910@gmail.com>
2024-02-29 11:33:09 +0530

    mobile drawer show request items

--- lib/screens/mobile/drawer.dart
+++ lib/screens/mobile/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:apidash/screens/home_page/collection_pane.dart';
 import 'package:apidash/screens/intro_page.dart';
 import 'package:apidash/screens/mobile/home_page/home_page.dart';
 import 'package:apidash/screens/settings_page.dart';
@@ -11,61 +12,68 @@ class MobileAppDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          ListTile(
-            title: const Text("Requests"),
-            leading: const Icon(
-              Icons.auto_awesome_mosaic,
-            ),
-            trailing: SizedBox(
-              width: 65,
-              child: Row(
-                children: [
-                  GestureDetector(
-                    child: const Icon(Icons.home_outlined),
-                    onTap: () {
-                      Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(
-                          builder: (context) => Scaffold(
-                            appBar: AppBar(
-                              title: const Text("Home"),
-                            ),
-                            drawer: const MobileAppDrawer(),
-                            body: const IntroPage(),
-                          ),
-                        ),
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                  ),
-                  const SizedBox(width: 5),
-                  GestureDetector(
-                    child: const Icon(Icons.settings_outlined),
-                    onTap: () {
-                      Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(
-                          builder: (context) => Scaffold(
-                            appBar: AppBar(
-                              title: const Text("Settings"),
-                            ),
-                            drawer: const MobileAppDrawer(),
-                            body: const SettingsPage(),
-                          ),
-                        ),
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                  ),
-                ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+            child: ListTile(
+              title: const Text("Requests"),
+              leading: const Icon(
+                Icons.auto_awesome_mosaic,
               ),
-            ),
-            onTap: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(
-                  builder: (context) => const MobileHomePage(),
+              trailing: SizedBox(
+                width: 65,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      child: const Icon(Icons.home_outlined),
+                      onTap: () {
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(
+                                title: const Text("Home"),
+                              ),
+                              drawer: const MobileAppDrawer(),
+                              body: const IntroPage(),
+                            ),
+                          ),
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                    ),
+                    const SizedBox(width: 5),
+                    GestureDetector(
+                      child: const Icon(Icons.settings_outlined),
+                      onTap: () {
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(
+                                title: const Text("Settings"),
+                              ),
+                              drawer: const MobileAppDrawer(),
+                              body: const SettingsPage(),
+                            ),
+                          ),
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                (Route<dynamic> route) => false,
-              );
-            },
+              ),
+              onTap: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => const MobileHomePage(),
+                  ),
+                  (Route<dynamic> route) => false,
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.95,
+            child: CollectionPane(),
           )
         ],
       ),
