0ba9e35e696399dc2eab6ddb024232a465ef5305
DILPREET1910 <dsahney1910@gmail.com>
2024-02-29 12:00:10 +0530

    dropdown button HTTP method in app bar

--- lib/screens/mobile/home_page/home_page.dart
+++ lib/screens/mobile/home_page/home_page.dart
@@ -1,22 +1,30 @@
+import 'package:apidash/screens/home_page/editor_pane/details_card/request_pane/request_pane.dart';
+import 'package:apidash/screens/home_page/editor_pane/url_card.dart';
 import 'package:apidash/screens/mobile/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MobileHomePage extends StatefulWidget {
+class MobileHomePage extends ConsumerWidget {
   const MobileHomePage({super.key});
 
   @override
-  State<MobileHomePage> createState() => _MobileHomePageState();
-}
-
-class _MobileHomePageState extends State<MobileHomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Reuqests"),
+        title: const Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 20,
+          ),
+          child: Row(
+            children: [
+              DropdownButtonHTTPMethod(),
+            ],
+          ),
+        ),
       ),
       drawer: const MobileAppDrawer(),
-      body: const Text("this"),
+      body: const EditRequestPane(),
     );
   }
 }
