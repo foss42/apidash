9aebce8d2b9eebccc4e419a71f20b28174d52761
Mohnish Deshpande <mohnish.deshpande1@gmail.com>
2024-02-29 17:20:32 +1100

    Write tests for csharp code generation

--- lib/codegen/csharp/http_client.dart
+++ lib/codegen/csharp/http_client.dart
@@ -14,12 +14,23 @@ class CSharpHttpClientCodeGen {
       case HTTPVerb.head:
         return "HttpMethod.Head";
       case HTTPVerb.patch:
-        return "HttpMethod.Path";
+        return "HttpMethod.Patch";
       case HTTPVerb.delete:
         return "HttpMethod.Delete";
       default:
         return "HttpMethod.Get";
     }
+  }
+
+  String getStringContent(String requestBody) {
+    String result = (requestBody).replaceAll('"', '\\"').replaceAll('\n', '');
+    result = '"$result"';
+    result += ', Encoding.UTF8, "application/json"';
+    return result;
+  }
+
+  bool postableMethod(HTTPVerb method) {
+    return method == HTTPVerb.post || method == HTTPVerb.put || method == HTTPVerb.patch;
   }
 
   String getCode(RequestModel requestModel, String defaultUriScheme) {
@@ -28,6 +39,7 @@ class CSharpHttpClientCodeGen {
 
       // Include necessary C# namespace
       result.writeln('using System;');
+      result.writeln('using System.Text;');
       result.writeln('using System.Net.Http;');
       result.writeln('using System.Threading.Tasks;\n');
 
@@ -43,24 +55,28 @@ class CSharpHttpClientCodeGen {
       // Create HttpRequestMessage
       result.writeln('    HttpRequestMessage request = new HttpRequestMessage(method, url);');
 
+      // Set request body if exists
+      var requestBody = requestModel.requestBody;
+      if (postableMethod(requestModel.method) && requestBody != null && requestBody.isNotEmpty) {
+        result.writeln(
+            '    request.Content = new StringContent(${getStringContent(requestBody)});\n');
+      } else {
+        result.writeln();
+      }
+
       // Set request headers
       var headersList = requestModel.enabledRequestHeaders;
       if (headersList != null && headersList.isNotEmpty) {
-        result.writeln('    HttpRequestHeaderCollection headers = request.Headers;');
+        result.writeln('    var headers = request.Headers;');
         for (var header in headersList) {
           result.writeln('    headers.Add("${header.name}", "${header.value}");');
         }
-      }
-
-      // Set request body if exists
-      var requestBody = requestModel.requestBody;
-      if (requestBody != null && requestBody.isNotEmpty) {
-        result.writeln('    request.Content = new StringContent("$requestBody");');
+        result.writeln();
       }
 
       // Send request and get response
       result.writeln('    HttpResponseMessage response = await client.SendAsync(request);');
-      result.writeln('    string responseBody = await response.Content.ReadAsStringAsync();');
+      result.writeln('    string responseBody = await response.Content.ReadAsStringAsync();\n');
       result.writeln('    Console.WriteLine(responseBody);');
       result.writeln('}');
 
---
+++ test/codegen/csharp_http_client_codegen_test.dart
@@ -0,0 +1,526 @@
+import 'package:apidash/codegen/csharp/http_client.dart';
+import 'package:apidash/codegen/python/http_client.dart';
+import '../request_models.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final csharpHttpClientCodeGen = CSharpHttpClientCodeGen();
+
+  group('GET Request', () {
+    test('GET 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet1, "https"), expectedCode);
+    });
+
+    test('GET 2', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/country/data";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet2, "https"), expectedCode);
+    });
+
+    test('GET 3', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/country/data?code=US";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet3, "https"), expectedCode);
+    });
+
+    test('GET 4', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/humanize/social";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet4, "https"), expectedCode);
+    });
+
+    test('GET 5', () {
+      const expectedCode = r"""using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.github.com/repos/foss42/apidash";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet5, "https"), expectedCode);
+    });
+
+    test('GET 6', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.github.com/repos/foss42/apidash";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet6, "https"), expectedCode);
+    });
+
+    test('GET 7', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet7, "https"), expectedCode);
+    });
+
+    test('GET 8', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.github.com/repos/foss42/apidash";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+    headers.Add("", "Bearer XYZ");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet8, "https"), expectedCode);
+    });
+
+    test('GET 9', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/humanize/social";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet9, "https"), expectedCode);
+    });
+
+    test('GET 10', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/humanize/social";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet10, "https"), expectedCode);
+    });
+
+    test('GET 11', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/humanize/social";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet11, "https"), expectedCode);
+    });
+
+    test('GET 12', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/humanize/social";
+HttpMethod method = HttpMethod.Get;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelGet12, "https"), expectedCode);
+    });
+  });
+
+  group('HEAD Request', () {
+    test('HEAD 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com";
+HttpMethod method = HttpMethod.Head;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelHead1, "https"), expectedCode);
+    });
+
+    test('HEAD 2', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "api.foss42.com";
+HttpMethod method = HttpMethod.Head;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelHead2, "http"), expectedCode);
+    });
+  });
+
+  group('POST Request', () {
+    test('POST 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/case/lower";
+HttpMethod method = HttpMethod.Post;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+    request.Content = new StringContent("{\"text\": \"I LOVE Flutter\"}", Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelPost1, "https"), expectedCode);
+    });
+
+    test('POST 2', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/case/lower";
+HttpMethod method = HttpMethod.Post;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+    request.Content = new StringContent("{\"text\": \"I LOVE Flutter\"}", Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelPost2, "https"), expectedCode);
+    });
+
+    test('POST 3', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://api.foss42.com/case/lower";
+HttpMethod method = HttpMethod.Post;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+    request.Content = new StringContent("{\"text\": \"I LOVE Flutter\"}", Encoding.UTF8, "application/json");
+
+    var headers = request.Headers;
+    headers.Add("User-Agent", "Test Agent");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelPost3, "https"), expectedCode);
+    });
+  });
+
+  group('PUT Request', () {
+    test('PUT 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://reqres.in/api/users/2";
+HttpMethod method = HttpMethod.Put;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+    request.Content = new StringContent("{\"name\": \"morpheus\",\"job\": \"zion resident\"}", Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelPut1, "https"), expectedCode);
+    });
+  });
+
+  group('PATCH Request', () {
+    test('PATCH 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://reqres.in/api/users/2";
+HttpMethod method = HttpMethod.Patch;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+    request.Content = new StringContent("{\"name\": \"marfeus\",\"job\": \"accountant\"}", Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelPatch1, "https"), expectedCode);
+    });
+  });
+
+  group('DELETE Request', () {
+    test('DELETE 1', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://reqres.in/api/users/2";
+HttpMethod method = HttpMethod.Delete;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelDelete1, "https"), expectedCode);
+    });
+
+    test('DELETE 2', () {
+      const expectedCode = r"""
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://reqres.in/api/users/2";
+HttpMethod method = HttpMethod.Delete;
+
+using (HttpClient client = new HttpClient())
+{
+    HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+""";
+      expect(csharpHttpClientCodeGen.getCode(requestModelDelete2, "https"), expectedCode);
+    });
+  });
+}
