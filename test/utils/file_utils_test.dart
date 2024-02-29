66ca7eee11924de666918b777b8d3cc011ab7fee
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2024-02-29 17:42:13 +0530

    [fix] breaking test due to platform separator char

--- test/utils/file_utils_test.dart
+++ test/utils/file_utils_test.dart
@@ -1,3 +1,4 @@
+import 'package:path/path.dart';
 import 'package:test/test.dart';
 import 'package:apidash/utils/file_utils.dart';
 
@@ -12,7 +13,15 @@ void main() {
 
       test('Test getShortPath', () {
         String path = "A/B/C/D.csv";
-        expect(getShortPath(path), ".../C/D.csv");
+        String shortPath = getShortPath(path);
+
+        // Default windows separator (\) causes issue with
+        // hardcoded default with separator for Linux/MacOS (/)
+        if (context.separator == '\\') {
+          shortPath = shortPath.replaceAll('\\', '/');
+        }
+
+        expect(shortPath, ".../C/D.csv");
       });
 
       test('Test getTempFileName', () {
