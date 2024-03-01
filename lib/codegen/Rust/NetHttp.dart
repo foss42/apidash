445fe4ee496b4b93b768642328dd29cf2207c765
nikkivirtuso <workynishant09@gmail.com>
2024-03-01 18:21:13 +0530

    correct format

--- lib/codegen/Rust/NetHttp.dart
+++ lib/codegen/Rust/NetHttp.dart
@@ -9,10 +9,10 @@ use std::io::Read;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::str;
+fn main() {
 ''';
 
   final String kTemplateUrl = '''
-
     let url = "{{url}}";
 
 ''';
@@ -21,6 +21,7 @@ use std::str;
 
     let mut url = "{{url}}".parse().unwrap();
 {{params}}
+
 ''';
 
   String kTemplateRequestBody = '''
@@ -30,17 +31,15 @@ use std::str;
 ''';
 
   final String kStringRequestStart = '''
-
     let mut stream = TcpStream::connect("{{host}}:{{port}}").unwrap();
     let mut request = String::new();
-    request.push_str("GET ");
+    request.push_str("{{method}} ");
     request.push_str(url.as_str());
-    request.push_str(" HTTP/1.1\r\n");
-    request.push_str("Host: ");
-    request.push_str("{{host}}");
-    request.push_str("\r\n");
-    request.push_str("Connection: close\r\n");
-    request.push_str("\r\n");
+    request.push_str(" HTTP/1.1\\r\\n");
+    request.push_str("Host: {{host}}\\r\\n ");
+    request.push_str("\\r\\n");
+    request.push_str("Connection: close\\r\\n");
+    request.push_str("\\r\\n");
     stream.write(request.as_bytes()).unwrap();
 
     let mut response = String::new();
@@ -50,64 +49,63 @@ use std::str;
 ''';
 
   String? getCode(
-    RequestModel requestModel,
-    String defaultUriScheme,
-  ) {
-    try {
-      String result = "";
-      bool hasQuery = false;
+  RequestModel requestModel,
+  String defaultUriScheme,
+) {
+  try {
+    String result = "";
+    bool hasQuery = false;
 
-      String url = requestModel.url;
-      if (!url.contains("://") && url.isNotEmpty) {
-        url = "$defaultUriScheme://$url";
-      }
-
-      var rec = getValidRequestUri(
-        url,
-        requestModel.enabledRequestParams,
-      );
-      Uri? uri = rec.$1;
-
-      if (uri != null) {
-        String url = stripUriParams(uri);
-
-        if (uri.hasQuery) {
-          var params = uri.queryParameters;
-          if (params.isNotEmpty) {
-            hasQuery = true;
-            var templateParams = jj.Template(kTemplateUrlQuery);
-            result += templateParams
-                .render({"url": url, "params": getQueryParams(params)});
-          }
-        }
-        if (!hasQuery) {
-          var templateUrl = jj.Template(kTemplateUrl);
-          result += templateUrl.render({"url": url});
-        }
-
-        var method = requestModel.method;
-        var requestBody = requestModel.requestBody;
-        if (kMethodsWithBody.contains(method) && requestBody != null) {
-          var templateBody = jj.Template(kTemplateRequestBody);
-          result += templateBody.render({"body": requestBody});
-        }
-
-        var templateStart = jj.Template(kTemplateStart);
-        result = templateStart.render({}) + result;
-
-        String host = uri.host;
-        int port = uri.port;
-
-        result = result.replaceAll("{{host}}", host);
-        result = result.replaceAll("{{port}}", port.toString());
-
-        result += kStringRequestStart;
-      }
-      return result;
-    } catch (e) {
-      return null;
+    String url = requestModel.url;
+    if (!url.contains("://") && url.isNotEmpty) {
+      url = "$defaultUriScheme://$url";
     }
+
+    var rec = getValidRequestUri(
+      url,
+      requestModel.enabledRequestParams,
+    );
+    Uri? uri = rec.$1;
+
+    if (uri != null) {
+      String url = stripUriParams(uri);
+
+      if (uri.hasQuery) {
+        var params = uri.queryParameters;
+        if (params.isNotEmpty) {
+          hasQuery = true;
+          var templateParams = jj.Template(kTemplateUrlQuery);
+          result += templateParams
+              .render({"url": url, "params": getQueryParams(params)});
+        }
+      }
+      if (!hasQuery) {
+        var templateUrl = jj.Template(kTemplateUrl);
+        result += templateUrl.render({"url": url});
+      }
+
+      var method = requestModel.method;
+      var additional = jj.Template(kStringRequestStart);
+      result += additional.render({
+        "method": method.name.toUpperCase(),
+        "host": uri.host,
+        "port": uri.port.toString(),
+      });
+
+      var requestBody = requestModel.requestBody;
+      if (kMethodsWithBody.contains(method) && requestBody != null) {
+        var templateBody = jj.Template(kTemplateRequestBody);
+        result += templateBody.render({"body": requestBody});
+      }
+
+      var templateStart = jj.Template(kTemplateStart);
+      result = templateStart.render({}) + result;
+    }
+    return result;
+  } catch (e) {
+    return null;
   }
+}
 
   String getQueryParams(Map<String, String> params) {
     String result = "";
