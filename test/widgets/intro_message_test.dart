1db5d88125d0d584d28f5cd21b1ed00b4c68be2f
Yousef Rabia <yousef.mohamed.rabia@gmail.com>
2024-03-01 17:20:40 +0200

    Ensure introData() Function Works During Testing by Using `setMockInitialValues`

--- test/widgets/intro_message_test.dart
+++ test/widgets/intro_message_test.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:apidash/widgets/intro_message.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
   testWidgets('Testing Intro Message', (tester) async {
+    PackageInfo.setMockInitialValues(
+        appName: 'API Dash',
+        packageName: 'dev.apidash.apidash',
+        version: '1.0.0',
+        buildNumber: '1',
+        buildSignature: 'buildSignature');
     await tester.pumpWidget(
       const MaterialApp(
         title: 'Intro Message',
