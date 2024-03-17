41ae457510a3deb7eec44a2d5e2c0e2b026b5f32
Shruti Roy <royshruti8237@gmail.com>
2024-03-17 23:19:52 +0530

    created php curl codegen file

---
+++ lib/codegen/php/curl.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'package:jinja/jinja.dart' as jj;
+import 'package:apidash/utils/utils.dart'
+    show getNewUuid, getValidRequestUri, requestModelToHARJsonRequest;
+import 'package:apidash/models/models.dart' show RequestModel;
+import 'package:apidash/consts.dart';
+
+class PHPcURLCodeGen {
+}
