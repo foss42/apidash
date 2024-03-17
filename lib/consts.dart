7c1a94fe4c3098eb7bfc61fd30257e4755df4de5
Shruti Roy <royshruti8237@gmail.com>
2024-03-18 00:12:02 +0530

    added php curl in the language options

--- lib/consts.dart
+++ lib/consts.dart
@@ -281,7 +281,8 @@ enum CodegenLanguage {
   javaAsyncHttpClient("Java (asynchttpclient)", "java", "java"),
   javaHttpClient("Java (HttpClient)", "java", "java"),
   juliaHttp("Julia (HTTP)", "julia", "jl"),
-  phpGuzzle("PHP (guzzle)", "php", "php");
+  phpGuzzle("PHP (guzzle)", "php", "php"),
+  phpCurl("PHP (curl)", "php", "php");
 
   const CodegenLanguage(this.label, this.codeHighlightLang, this.ext);
   final String label;
