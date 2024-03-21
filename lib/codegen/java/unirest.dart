75b5853c3a385ebe7ff3029cbac64dc200b49958
Aditya Mayukh Som <adityamayukhsom@gmail.com>
2024-03-21 17:44:27 +0530

    added JavaUnirestCodeGen class

---
+++ lib/codegen/java/unirest.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'package:apidash/utils/har_utils.dart';
+import 'package:apidash/utils/http_utils.dart';
+import 'package:jinja/jinja.dart' as jj;
+import 'package:apidash/models/models.dart' show RequestModel;
+import 'package:apidash/consts.dart';
+
+class JavaUnirestGen {
+}
