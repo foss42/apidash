2823174721943b5cc29bf8d42e71f93161f99b74
Shruti Roy <royshruti8237@gmail.com>
2024-03-23 05:41:56 +0530

    testing for java httpClient codegen

---
+++ test/codegen/java_httpclient_codegen_test.dart
@@ -0,0 +1,9 @@
+import 'package:apidash/codegen/codegen.dart';
+import 'package:apidash/consts.dart';
+import 'package:test/test.dart';
+import '../request_models.dart';
+
+void main() {
+  final codeGen = Codegen();
+
+}
