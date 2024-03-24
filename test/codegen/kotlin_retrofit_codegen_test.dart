d5ed9963d66511251d1fcc81caf477ab2974f3a9
Shivam Verma <100941430+Photon3009@users.noreply.github.com>
2024-03-24 18:03:43 +0530

    Updated kotlin_retrofit_codegen_test.dart

--- test/codegen/kotlin_retrofit_codegen_test.dart
+++ test/codegen/kotlin_retrofit_codegen_test.dart
@@ -87,12 +87,347 @@ fun main() {
 ''';
       expect(
           codeGen.getCode(
-              CodegenLanguage.kotlinRetrofit, requestModelGet2, "https"),
+              CodegenLanguage.kotlinRetrofit, requestModelGet3, "https"),
+          expectedCode);
+    });
+
+    test('GET 4', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/humanize/social", @Query("num") num: String, @Query("digits") digits: String, @Query("system") system: String, @Query("add_space") add_space: String, @Query("trailing_zeros") trailing_zeros: String)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet4, "https"),
+          expectedCode);
+    });
+
+    test('GET 5', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/repos/foss42/apidash")@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet5, "https"),
+          expectedCode);
+    });
+
+    test('GET 6', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/repos/foss42/apidash", @Query("raw") raw: String)@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet6, "https"),
+          expectedCode);
+    });
+
+    test('GET 7', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet7, "https"),
+          expectedCode);
+    });
+
+    test('GET 8', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/repos/foss42/apidash", @Query("raw") raw: String)@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet8, "https"),
+          expectedCode);
+    });
+
+    test('GET 9', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/humanize/social", @Query("num") num: String, @Query("add_space") add_space: String)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet9, "https"),
+          expectedCode);
+    });
+
+    test('GET 10', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/humanize/social")@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet10, "https"),
+          expectedCode);
+    });
+
+    test('GET 11', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/humanize/social", @Query("num") num: String, @Query("digits") digits: String)@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet11, "https"),
+          expectedCode);
+    });
+
+    test('GET 12', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @GET("/humanize/social")
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelGet12, "https"),
           expectedCode);
     });
   });
 
   group('POST Request', () {
+    test('POST 1', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @POST("/case/lower", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelPost1, "https"),
+          expectedCode);
+    });
+
+    test('POST 2', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @POST("/case/lower", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelPost2, "https"),
+          expectedCode);
+    });
+
+    test('POST 3', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @POST("/case/lower", @Body requestBody: RequestBody)@Header("Test Agent") User-Agent: String
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.apidash.dev")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelPost3, "https"),
+          expectedCode);
+    });
     test('POST 4', () {
       const expectedCode = '''
 import retrofit2.http.*
@@ -264,10 +599,119 @@ fun main() {
   });
 
   group('PUT Request', () {
-    // Add test cases for PUT requests...
+    test('Put 1', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @PUT("/api/users/2", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://reqres.in")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelPut1, "https"),
+          expectedCode);
+    });
   });
 
+  group('PATCH Request', () {
+    test('Patch 1', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @PATCH("/api/users/2", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://reqres.in")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelPatch1, "https"),
+          expectedCode);
+    });
+  });
   group('DELETE Request', () {
-    // Add test cases for DELETE requests...
+    test('Delete 1', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @DELETE("/api/users/2")
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://reqres.in")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelDelete1, "https"),
+          expectedCode);
+    });
+
+    test('Delete 2', () {
+      const expectedCode = '''
+import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ApiService {
+    @DELETE("/api/users/2", @Body requestBody: RequestBody)
+    fun (): Call<ResponseBody>
+}
+
+fun main() {
+    val service = Retrofit.Builder()
+        .baseUrl("https://reqres.in")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+
+    // Call your API methods here using service object
+}
+''';
+      expect(
+          codeGen.getCode(
+              CodegenLanguage.kotlinRetrofit, requestModelDelete2, "https"),
+          expectedCode);
+    });
   });
 }
