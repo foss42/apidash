17a171d856951b8ddf16d01b9eb7f3af220c1152
Yashas H Majmudar <yashashm.dev@gmail.com>
2024-03-24 14:18:30 +0530

    fix: UI changes

--- lib/screens/home_page/editor_pane/editor_request.dart
+++ lib/screens/home_page/editor_pane/editor_request.dart
@@ -18,8 +18,8 @@ class RequestEditor extends StatelessWidget {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // RequestEditorTopBar(),
-        // kVSpacer5,
+        RequestEditorTopBar(),
+        kVSpacer5,
         EditorPaneRequestURLCard(),
         kVSpacer10,
         Expanded(
@@ -58,124 +58,60 @@ class _RequestTabViewState extends ConsumerState<RequestTabView> {
     final requestItems = ref.watch(collectionStateNotifierProvider)!;
     final alwaysShowCollectionPaneScrollbar = ref.watch(settingsProvider
         .select((value) => value.alwaysShowCollectionPaneScrollbar));
-    final id = ref.watch(selectedIdStateProvider);
-    final name = ref.watch(selectedRequestModelProvider.select((value) => value?.name));
 
-    return Row(
-      children: [
-        Expanded(
-          child: Scrollbar(
-            controller: controller,
-            thumbVisibility: alwaysShowCollectionPaneScrollbar ? true : null,
-            thickness: 5,
-            radius: const Radius.circular(12),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              constraints: const BoxConstraints(
-                maxHeight: 42,
-                minHeight: 42,
-              ),
-              padding: const EdgeInsets.only(bottom: 7),
-              child: ReorderableListView.builder(
-                physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
-                scrollController: controller,
-                scrollDirection: Axis.horizontal,
-                buildDefaultDragHandles: false,
-                itemCount: tabSequence.length,
-                onReorder: (int oldIndex, int newIndex) {
-                  if (oldIndex < newIndex) {
-                    newIndex -= 1;
-                  }
-                  if (oldIndex != newIndex) {
-                    ref
-                        .read(collectionStateNotifierProvider.notifier)
-                        .reorderTab(oldIndex, newIndex);
-                  }
-                },
-                itemExtentBuilder: (index, dimensions) {
-                  final length = dimensions.viewportMainAxisExtent / tabSequence.length;
+    return Scrollbar(
+      controller: controller,
+      thumbVisibility: alwaysShowCollectionPaneScrollbar ? true : null,
+      thickness: 5,
+      radius: const Radius.circular(12),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        constraints: const BoxConstraints(
+          maxHeight: 42,
+          minHeight: 42,
+        ),
+        padding: const EdgeInsets.only(bottom: 7),
+        child: ReorderableListView.builder(
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          scrollController: controller,
+          scrollDirection: Axis.horizontal,
+          buildDefaultDragHandles: false,
+          itemCount: tabSequence.length,
+          onReorder: (int oldIndex, int newIndex) {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            if (oldIndex != newIndex) {
+              ref
+                  .read(collectionStateNotifierProvider.notifier)
+                  .reorderTab(oldIndex, newIndex);
+            }
+          },
+          itemExtentBuilder: (index, dimensions) {
+            final length = dimensions.viewportMainAxisExtent / tabSequence.length;
 
-                  if (length > dimensions.viewportMainAxisExtent / 6) {
-                    return dimensions.viewportMainAxisExtent / 6;
-                  } else if (length < dimensions.viewportMainAxisExtent / 8) {
-                    return dimensions.viewportMainAxisExtent / 8;
-                  } else {
-                    return length;
-                  }
-                },
-                itemBuilder: (context, index) {
-                  var id = tabSequence[index];
-                  return ReorderableDragStartListener(
-                    key: ValueKey(id),
-                    index: index,
-                    child: TabItem(
-                      id: id,
-                      requestModel: requestItems[id]!,
-                    ),
-                  );
-                },
+            if (length > dimensions.viewportMainAxisExtent / 6) {
+              return dimensions.viewportMainAxisExtent / 6;
+            } else if (length < dimensions.viewportMainAxisExtent / 8) {
+              return dimensions.viewportMainAxisExtent / 8;
+            } else {
+              return length;
+            }
+          },
+          itemBuilder: (context, index) {
+            var id = tabSequence[index];
+            return ReorderableDragStartListener(
+              key: ValueKey(id),
+              index: index,
+              child: TabItem(
+                id: id,
+                requestModel: requestItems[id]!,
               ),
-            ),
-          ),
+            );
+          },
         ),
-        kHSpacer10,
-        SizedBox(
-          width: 90,
-          height: 24,
-          child: FilledButton.tonalIcon(
-            style: const ButtonStyle(
-              padding: MaterialStatePropertyAll(EdgeInsets.zero),
-            ),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    final controller =
-                    TextEditingController(text: name ?? "");
-                    controller.selection = TextSelection(
-                        baseOffset: 0, extentOffset: controller.text.length);
-                    return AlertDialog(
-                      title: const Text('Rename Request'),
-                      content: TextField(
-                        autofocus: true,
-                        controller: controller,
-                        decoration:
-                        const InputDecoration(hintText: "Enter new name"),
-                      ),
-                      actions: <Widget>[
-                        OutlinedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('CANCEL')),
-                        FilledButton(
-                            onPressed: () {
-                              final val = controller.text.trim();
-                              ref
-                                  .read(collectionStateNotifierProvider
-                                  .notifier)
-                                  .update(id!, name: val);
-                              Navigator.pop(context);
-                              controller.dispose();
-                            },
-                            child: const Text('OK')),
-                      ],
-                    );
-                  });
-            },
-            icon: const Icon(
-              Icons.edit,
-              size: 12,
-            ),
-            label: Text(
-              "Rename",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ),
-        ),
-        kHSpacer10,
-      ],
+      ),
     );
   }
 }
