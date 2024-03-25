935c9c03d5124bc181216376bffe4a549210d266
Tanish2002 <tanishkhare@gmail.com>
2024-03-25 18:21:16 +0530

    feat: syntax highlight inside json request body

--- lib/widgets/editor_json.dart
+++ lib/widgets/editor_json.dart
@@ -1,7 +1,8 @@
-import 'dart:math' as math;
+import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:json_text_field/json_text_field.dart';
+import 'package:highlight/languages/json.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+import 'package:flutter_highlight/themes/default.dart';
 import 'package:apidash/consts.dart';
 
 class JsonTextFieldEditor extends StatefulWidget {
@@ -20,36 +21,31 @@ class JsonTextFieldEditor extends StatefulWidget {
 }
 
 class _JsonTextFieldEditorState extends State<JsonTextFieldEditor> {
-  final JsonTextFieldController controller = JsonTextFieldController();
+  final TextEditingController controller = TextEditingController();
   late final FocusNode editorFocusNode;
-
-  void insertTab() {
-    String sp = "  ";
-    int offset = math.min(
-        controller.selection.baseOffset, controller.selection.extentOffset);
-    String text = controller.text.substring(0, offset) +
-        sp +
-        controller.text.substring(offset);
-    controller.value = TextEditingValue(
-      text: text,
-      selection: controller.selection.copyWith(
-        baseOffset: controller.selection.baseOffset + sp.length,
-        extentOffset: controller.selection.extentOffset + sp.length,
-      ),
-    );
-    widget.onChanged?.call(text);
-  }
+  CodeController? _codeController;
+  bool _focused = false;
 
   @override
   void initState() {
     super.initState();
-    controller.formatJson(sortJson: false);
     editorFocusNode = FocusNode(debugLabel: "Editor Focus Node");
+    _codeController = CodeController(
+      text: widget.initialValue,
+      language: json,
+    );
+    // listener for changing border color on focus change
+    editorFocusNode.addListener(() {
+      setState(() {
+        _focused = editorFocusNode.hasFocus;
+      });
+    });
   }
 
   @override
   void dispose() {
     editorFocusNode.dispose();
+    _codeController?.dispose();
     super.dispose();
   }
 
@@ -58,69 +54,41 @@ class _JsonTextFieldEditorState extends State<JsonTextFieldEditor> {
     if (widget.initialValue != null) {
       controller.text = widget.initialValue!;
     }
-    return CallbackShortcuts(
-      bindings: <ShortcutActivator, VoidCallback>{
-        const SingleActivator(LogicalKeyboardKey.tab): () {
-          insertTab();
-        },
-      },
-      child: JsonTextField(
-        stringHighlightStyle: kCodeStyle.copyWith(
-          color: Theme.of(context).colorScheme.secondary,
-        ),
-        keyHighlightStyle: kCodeStyle.copyWith(
-          color: Theme.of(context).colorScheme.primary,
-          fontWeight: FontWeight.bold,
-        ),
-        errorContainerDecoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.error.withOpacity(
-                kForegroundOpacity,
-              ),
-          borderRadius: kBorderRadius8,
-        ),
-        showErrorMessage: true,
-        isFormatting: true,
+    return CodeTheme(
+      data: CodeThemeData(
+          styles: Theme.of(context).brightness == Brightness.dark
+              ? monokaiSublimeTheme
+              : defaultTheme),
+      child: CodeField(
         key: Key(widget.fieldKey),
-        controller: controller,
+        controller: _codeController!,
         focusNode: editorFocusNode,
         keyboardType: TextInputType.multiline,
         expands: true,
         maxLines: null,
-        style: kCodeStyle,
-        textAlignVertical: TextAlignVertical.top,
-        onChanged: (value) {
-          controller.formatJson(sortJson: false);
-          widget.onChanged?.call(value);
-        },
-        decoration: InputDecoration(
-          hintText: "Enter content (body)",
-          hintStyle: TextStyle(
-            color: Theme.of(context).colorScheme.outline.withOpacity(
-                  kHintOpacity,
-                ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: kBorderRadius8,
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.primary.withOpacity(
-                    kHintOpacity,
-                  ),
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: kBorderRadius8,
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.surfaceVariant,
-            ),
-          ),
-          filled: true,
-          hoverColor: kColorTransparent,
-          fillColor: Color.alphaBlend(
+        textStyle: kCodeStyle,
+        lineNumbers: false,
+        hintText: "Enter content (json)",
+        hintStyle: TextStyle(
+          color: Theme.of(context).colorScheme.outline.withOpacity(
+                kHintOpacity,
+              ),
+        ),
+        onChanged: widget.onChanged,
+        decoration: BoxDecoration(
+          color: Color.alphaBlend(
               (Theme.of(context).brightness == Brightness.dark
                       ? Theme.of(context).colorScheme.onPrimaryContainer
                       : Theme.of(context).colorScheme.primaryContainer)
                   .withOpacity(kForegroundOpacity),
               Theme.of(context).colorScheme.surface),
+          border: Border.fromBorderSide(BorderSide(
+              color: _focused
+                  ? Theme.of(context).colorScheme.primary.withOpacity(
+                        kHintOpacity,
+                      )
+                  : Theme.of(context).colorScheme.surfaceVariant)),
+          borderRadius: kBorderRadius8,
         ),
       ),
     );
