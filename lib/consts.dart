e449ced99b0347059771e6be00746b4399d702ae
adityamayukhsom <adityamayukhsom@gmail.com>
2024-03-28 03:51:41 +0530

    added ruby faraday as constant

--- lib/consts.dart
+++ lib/consts.dart
@@ -289,6 +289,7 @@ enum CodegenLanguage {
   kotlinOkHttp("Kotlin (okhttp3)", "java", "kt"),
   pythonRequests("Python (requests)", "python", "py"),
   pythonHttpClient("Python (http.client)", "python", "py"),
+  rubyFaraday("Ruby (faraday)", "ruby", "rb"),
   rustActix("Rust (Actix Client)", "rust", "rs"),
   rustReqwest("Rust (reqwest)", "rust", "rs"),
   rustCurl("Rust (curl-rust)", "rust", "rs"),
