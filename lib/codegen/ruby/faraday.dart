b86adacffcfce660b81b3f6c4ea0ab3e858a18fa
adityamayukhsom <adityamayukhsom@gmail.com>
2024-03-31 19:29:00 +0530

    addec class for generating faraday code

---
+++ lib/codegen/ruby/faraday.dart
@@ -0,0 +1,14 @@
+import 'package:apidash/consts.dart';
+import 'package:jinja/jinja.dart' as jj;
+import 'package:apidash/utils/utils.dart' show getValidRequestUri;
+import 'package:apidash/utils/http_utils.dart' show stripUriParams;
+
+import 'package:apidash/models/models.dart' show RequestModel;
+
+// Note that delete is a special case in Faraday as API Dash supports request
+// body inside delete reqest, but Faraday does not. Hence we need to manually
+// setup request body for delete request and add that to request.
+//
+// Refer https://lostisland.github.io/faraday/#/getting-started/quick-start?id=get-head-delete-trace
+class RubyFaradayCodeGen {
+}
