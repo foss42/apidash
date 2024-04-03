5e5eb747b486dcf47fc2834d4bdbe55f7c11771b
adityamayukhsom <adityamayukhsom@gmail.com>
2024-04-03 12:52:01 +0530

    [ISSUE 375] - fixed imports during code generation

--- lib/codegen/php/curl.dart
+++ lib/codegen/php/curl.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 import 'package:jinja/jinja.dart' as jj;
-import 'package:apidash/utils/utils.dart'
-    show getNewUuid, getValidRequestUri, requestModelToHARJsonRequest;
+import 'package:apidash/utils/utils.dart' show getValidRequestUri, stripUriParams;
 import 'package:apidash/models/models.dart' show RequestModel;
 import 'package:apidash/consts.dart';
 
