c6c215ace138e22abf5bd1e508ba4f2386c070a2
Ashita Prasad <ashitaprasad92@gmail.com>
2024-09-16 17:02:43 +0530

    Update request_body.dart

--- lib/screens/home_page/editor_pane/details_card/request_pane/request_body.dart
+++ lib/screens/home_page/editor_pane/details_card/request_pane/request_body.dart
@@ -1,4 +1,3 @@
-import 'package:apidash/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:apidash/providers/providers.dart';
@@ -18,19 +17,20 @@ class EditRequestBody extends ConsumerWidget {
     final contentType = ref.watch(selectedRequestModelProvider
         .select((value) => value?.httpRequestModel?.bodyContentType));
 
-    final sm = ScaffoldMessenger.of(context);
-    void changeToPostMethod() {
-      if (requestModel?.httpRequestModel!.method == HTTPVerb.get) {
-        ref
-            .read(collectionStateNotifierProvider.notifier)
-            .update(selectedId, method: HTTPVerb.post);
-        sm.hideCurrentSnackBar();
-        sm.showSnackBar(getSnackBar(
-          "Switched to POST method",
-          small: false,
-        ));
-      }
-    }
+    // TODO: #178 GET->POST Currently switches to POST everytime user edits body even if the user intentionally chooses GET
+    // final sm = ScaffoldMessenger.of(context);
+    // void changeToPostMethod() {
+    //   if (requestModel?.httpRequestModel!.method == HTTPVerb.get) {
+    //     ref
+    //         .read(collectionStateNotifierProvider.notifier)
+    //         .update(selectedId, method: HTTPVerb.post);
+    //     sm.hideCurrentSnackBar();
+    //     sm.showSnackBar(getSnackBar(
+    //       "Switched to POST method",
+    //       small: false,
+    //     ));
+    //   }
+    // }
 
     return Column(
       children: [
@@ -48,11 +48,12 @@ class EditRequestBody extends ConsumerWidget {
         ),
         Expanded(
           child: switch (contentType) {
-            ContentType.formdata => Padding(
+            ContentType.formdata => const Padding(
                 padding: kPh4,
                 child: FormDataWidget(
-                  changeMethodToPost: changeToPostMethod,
-                )),
+                    // TODO: See changeToPostMethod above
+                    // changeMethodToPost: changeToPostMethod,
+                    )),
             // TODO: Fix JsonTextFieldEditor & plug it here
             ContentType.json => Padding(
                 padding: kPt5o10,
@@ -61,7 +62,7 @@ class EditRequestBody extends ConsumerWidget {
                   fieldKey: "$selectedId-json-body-editor",
                   initialValue: requestModel?.httpRequestModel?.body,
                   onChanged: (String value) {
-                    changeToPostMethod();
+                    // changeToPostMethod();
                     ref
                         .read(collectionStateNotifierProvider.notifier)
                         .update(selectedId, body: value);
@@ -75,7 +76,7 @@ class EditRequestBody extends ConsumerWidget {
                   fieldKey: "$selectedId-body-editor",
                   initialValue: requestModel?.httpRequestModel?.body,
                   onChanged: (String value) {
-                    changeToPostMethod();
+                    // changeToPostMethod();
                     ref
                         .read(collectionStateNotifierProvider.notifier)
                         .update(selectedId, body: value);
