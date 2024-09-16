28a22218fcc5be9a998f47eabbc39b7a1f89a659
Ashita Prasad <ashitaprasad92@gmail.com>
2024-09-17 02:30:58 +0530

    Fixes  #415

--- lib/utils/http_utils.dart
+++ lib/utils/http_utils.dart
@@ -101,33 +101,34 @@ String stripUrlParams(String url) {
 
 (List<ResponseBodyView>, String?) getResponseBodyViewOptions(
     MediaType? mediaType) {
-  if (mediaType != null) {
-    var type = mediaType.type;
-    var subtype = mediaType.subtype;
-    if (kResponseBodyViewOptions.containsKey(type)) {
-      if (kResponseBodyViewOptions[type]!.containsKey(subtype)) {
-        return (
-          kResponseBodyViewOptions[type]![subtype]!,
-          kCodeHighlighterMap[subtype] ?? subtype
-        );
-      }
-      if (subtype.contains(kSubTypeJson)) {
-        subtype = kSubTypeJson;
-      }
-      if (subtype.contains(kSubTypeXml)) {
-        subtype = kSubTypeXml;
-      }
-      if (kResponseBodyViewOptions[type]!.containsKey(subtype)) {
-        return (
-          kResponseBodyViewOptions[type]![subtype]!,
-          kCodeHighlighterMap[subtype] ?? subtype
-        );
-      }
+  if (mediaType == null) {
+    return (kRawBodyViewOptions, null);
+  }
+  var type = mediaType.type;
+  var subtype = mediaType.subtype;
+  if (kResponseBodyViewOptions.containsKey(type)) {
+    if (kResponseBodyViewOptions[type]!.containsKey(subtype)) {
       return (
-        kResponseBodyViewOptions[type]![kSubTypeDefaultViewOptions]!,
-        subtype
+        kResponseBodyViewOptions[type]![subtype]!,
+        kCodeHighlighterMap[subtype] ?? subtype
       );
     }
+    if (subtype.contains(kSubTypeJson)) {
+      subtype = kSubTypeJson;
+    }
+    if (subtype.contains(kSubTypeXml)) {
+      subtype = kSubTypeXml;
+    }
+    if (kResponseBodyViewOptions[type]!.containsKey(subtype)) {
+      return (
+        kResponseBodyViewOptions[type]![subtype]!,
+        kCodeHighlighterMap[subtype] ?? subtype
+      );
+    }
+    return (
+      kResponseBodyViewOptions[type]![kSubTypeDefaultViewOptions]!,
+      subtype
+    );
   }
   return (kNoBodyViewOptions, null);
 }
