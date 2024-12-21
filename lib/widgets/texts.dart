d7566716ba89b315cf52eaf3491c72851d37a393
Ashita Prasad <ashitaprasad92@gmail.com>
2024-12-22 02:38:40 +0530

    Update HTTPVerb

--- lib/widgets/texts.dart
+++ lib/widgets/texts.dart
@@ -4,22 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:apidash/utils/utils.dart';
 
 class MethodBox extends StatelessWidget {
-  const MethodBox({super.key, required this.method});
+  const MethodBox({
+    super.key,
+    required this.method,
+  });
   final HTTPVerb method;
 
   @override
   Widget build(BuildContext context) {
-    String text = method.name.toUpperCase();
-    if (method == HTTPVerb.delete) {
-      text = "DEL";
-    }
-    if (method == HTTPVerb.patch) {
-      text = "PAT";
-    }
     return SizedBox(
       width: 24,
       child: Text(
-        text,
+        method.abbr,
         textAlign: TextAlign.center,
         style: TextStyle(
           fontSize: 8,
