26979fe6761a96e067640387d0fec3c5d9933865
Ashita Prasad <ashitaprasad92@gmail.com>
2025-02-19 03:38:29 +0530

    Update tests

--- test/widgets/dialog_proxy_settings_test.dart
+++ test/widgets/dialog_proxy_settings_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:apidash/widgets/proxy_settings_dialog.dart';
 
 void main() {
   testWidgets('ProxySettingsDialog shows correctly', (WidgetTester tester) async {
