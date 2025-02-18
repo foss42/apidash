23816ee0be4d022c5426cee9827eeb264629d0d4
Ashita Prasad <ashitaprasad92@gmail.com>
2025-02-19 03:38:49 +0530

    Update settings_page.dart

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,4 +1,3 @@
-import 'package:apidash/widgets/proxy_settings_dialog.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
