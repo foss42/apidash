d56e99dac34b331d73e6ffd40cf47c2b600c83a2
Ashita Prasad <ashitaprasad92@gmail.com>
2025-03-02 17:49:42 +0530

    Update video_previewer.dart

--- lib/widgets/video_previewer.dart
+++ lib/widgets/video_previewer.dart
@@ -19,16 +19,17 @@ class VideoPreviewer extends StatefulWidget {
 }
 
 class _VideoPreviewerState extends State<VideoPreviewer> {
-  VideoPlayerController? _videoController;
+  late VideoPlayerController _videoController;
+  late Future<void> _initializeVideoPlayerFuture;
   bool _isPlaying = false;
-  File? _tempVideoFile;
+  late File _tempVideoFile;
   bool _showControls = false;
 
   @override
   void initState() {
     super.initState();
     registerWithAllPlatforms();
-    _initializeVideoPlayer();
+    _initializeVideoPlayerFuture = _initializeVideoPlayer();
   }
 
   void registerWithAllPlatforms() {
@@ -39,21 +40,20 @@ class _VideoPreviewerState extends State<VideoPreviewer> {
     }
   }
 
-  void _initializeVideoPlayer() async {
+  Future<void> _initializeVideoPlayer() async {
     final tempDir = await getTemporaryDirectory();
     _tempVideoFile = File(
         '${tempDir.path}/temp_video_${DateTime.now().millisecondsSinceEpoch}');
     try {
-      await _tempVideoFile?.writeAsBytes(widget.videoBytes);
-      _videoController = VideoPlayerController.file(_tempVideoFile!)
-        ..initialize().then((_) {
-          if (mounted) {
-            setState(() {
-              _videoController!.play();
-              _videoController!.setLooping(true);
-            });
-          }
+      await _tempVideoFile.writeAsBytes(widget.videoBytes);
+      _videoController = VideoPlayerController.file(_tempVideoFile);
+      await _videoController.initialize();
+      if (mounted) {
+        setState(() {
+          _videoController.play();
+          _videoController.setLooping(true);
         });
+      }
     } catch (e) {
       debugPrint("VideoPreviewer _initializeVideoPlayer(): $e");
       return;
@@ -69,74 +69,78 @@ class _VideoPreviewerState extends State<VideoPreviewer> {
       backgroundColor: iconColor.withValues(alpha: 0.3),
     );
     return Scaffold(
-      body: MouseRegion(
-        onEnter: (_) => setState(() => _showControls = true),
-        onExit: (_) => setState(() => _showControls = false),
-        child: Stack(
-          children: [
-            Center(
-              child: _videoController?.value.isInitialized == true
-                  ? AspectRatio(
-                      aspectRatio: _videoController!.value.aspectRatio,
-                      child: VideoPlayer(_videoController!),
-                    )
-                  : const CircularProgressIndicator(),
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: _videoController?.value.isInitialized == true
-                  ? SizedBox(
-                      height: 50.0,
-                      child: VideoProgressIndicator(
-                        _videoController!,
-                        allowScrubbing: true,
-                        padding: const EdgeInsets.all(20),
-                        colors: progressBarColors,
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (_videoController.value.isInitialized) {
+              return MouseRegion(
+                onEnter: (_) => setState(() => _showControls = true),
+                onExit: (_) => setState(() => _showControls = false),
+                child: Stack(
+                  children: [
+                    Center(
+                      child: AspectRatio(
+                        aspectRatio: _videoController.value.aspectRatio,
+                        child: VideoPlayer(_videoController),
                       ),
-                    )
-                  : Container(height: 0),
-            ),
-            if (_showControls)
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    if (_videoController!.value.isPlaying) {
-                      _videoController!.pause();
-                    } else {
-                      _videoController!.play();
-                    }
-                    setState(() {
-                      _isPlaying = !_isPlaying;
-                    });
-                  },
-                  child: Container(
-                    color: Colors.transparent,
-                    child: Icon(
-                      _isPlaying ? Icons.play_arrow : Icons.pause,
-                      size: 64,
-                      color: iconColor,
                     ),
-                  ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: SizedBox(
+                        height: 50.0,
+                        child: VideoProgressIndicator(
+                          _videoController,
+                          allowScrubbing: true,
+                          padding: const EdgeInsets.all(20),
+                          colors: progressBarColors,
+                        ),
+                      ),
+                    ),
+                    if (_showControls)
+                      Center(
+                        child: GestureDetector(
+                          onTap: () {
+                            if (_videoController.value.isPlaying) {
+                              _videoController.pause();
+                            } else {
+                              _videoController.play();
+                            }
+                            setState(() {
+                              _isPlaying = !_isPlaying;
+                            });
+                          },
+                          child: Container(
+                            color: Colors.transparent,
+                            child: Icon(
+                              _isPlaying ? Icons.play_arrow : Icons.pause,
+                              size: 64,
+                              color: iconColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
                 ),
-              ),
-          ],
-        ),
+              );
+            }
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
 
   @override
   void dispose() {
-    _videoController?.pause();
-    _videoController?.dispose();
+    _videoController.pause();
+    _videoController.dispose();
     if (!kIsRunningTests) {
       Future.delayed(const Duration(seconds: 1), () async {
         try {
-          if (_tempVideoFile != null) {
-            await _tempVideoFile!.delete();
-          }
+          await _tempVideoFile.delete();
         } catch (e) {
           debugPrint("VideoPreviewer dispose(): $e");
           return;
