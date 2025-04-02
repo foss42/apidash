07449e4542231c7eeefedd692d569f355101fe94
Udhay-Adithya <udhayxd@gmail.com>
2025-04-02 13:31:01 +0530

    feat: enhance chat response with loading indicator and markdown support

--- lib/dashbot/features/home/view/widgets/chat_message.dart
+++ lib/dashbot/features/home/view/widgets/chat_message.dart
@@ -1,4 +1,7 @@
+// chat_message.dart
+import 'package:apidash_design_system/tokens/tokens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart'; // Import the package
 
 class ChatMessage {
   final String text;
@@ -15,21 +18,62 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (message.isEmpty) {
+      return Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          children: [
+            kVSpacer8,
+            Image.asset(
+              "assets/dashbot_icon_1.png",
+              width: 42,
+            ),
+            kVSpacer8,
+            CircularProgressIndicator.adaptive(),
+          ],
+        ),
+      );
+    }
     return Align(
       alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 5.0),
-        padding: const EdgeInsets.all(12.0),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.7,
-        ),
-        decoration: BoxDecoration(
-          color: isUser
-              ? Theme.of(context).colorScheme.primaryContainer
-              : Theme.of(context).colorScheme.surfaceContainer,
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: Text(message),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (!isUser) ...[
+            kVSpacer6,
+            Image.asset(
+              "assets/dashbot_icon_1.png",
+              width: 42,
+            ),
+            kVSpacer8,
+          ],
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 5.0),
+            padding: const EdgeInsets.all(12.0),
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.75,
+            ),
+            decoration: BoxDecoration(
+              color: isUser
+                  ? Theme.of(context).colorScheme.primaryContainer
+                  : Theme.of(context).colorScheme.surfaceContainerHighest,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: MarkdownBody(
+              data: message.isEmpty ? " " : message,
+              selectable: true,
+              styleSheet:
+                  MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                p: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: isUser
+                          ? Theme.of(context).colorScheme.surfaceBright
+                          : Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
