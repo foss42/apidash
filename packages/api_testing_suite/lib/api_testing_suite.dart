881a5719c468384b5ec7811502b06ea68f997fef
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2025-04-10 17:15:49 +0530

    fix: update export paths for workflow builder widgets and reorganize model exports

--- packages/api_testing_suite/lib/api_testing_suite.dart
+++ packages/api_testing_suite/lib/api_testing_suite.dart
@@ -7,7 +7,12 @@ export 'src/models/workflow_model.dart';
 export 'src/providers/workflow_providers.dart';
 
 // Widgets
-export 'src/widgets/workflow_builder/workflow_canvas.dart';
-export 'src/widgets/workflow_builder/workflow_connection.dart';
-export 'src/widgets/workflow_builder/workflow_node.dart';
-export 'src/widgets/workflow_builder/workflow_screens.dart';
+export 'src/workflow_builder/workflow_canvas.dart';
+export 'src/workflow_builder/workflow_connection.dart';
+export 'src/workflow_builder/workflow_node.dart';
+export 'src/workflow_builder/workflow_screens.dart';
+
+// Models
+export 'src/stress_test/models/stress_test_config.dart';
+export 'src/stress_test/models/stress_test_summary.dart';
+export 'src/fake_data_provider/models/fake_data_config.dart';
