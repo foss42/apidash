65a82aaa78d683d2371395816cf00f91eb62bbc4
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2025-04-10 14:29:33 +0530

    refactor: reorder NodeStatus enum values and update extension for color mapping

--- packages/api_testing_suite/lib/src/models/node_status.dart
+++ packages/api_testing_suite/lib/src/models/node_status.dart
@@ -1,8 +1,27 @@
+import 'package:flutter/material.dart';
+
 /// Enum representing the different states a workflow node can be in
 enum NodeStatus {
-  pending,
+  inactive,
   running,
   success,
   failure,
-  inactive,
+  pending,
+}
+
+extension NodeStatusExtension on NodeStatus {
+  Color get color {
+    switch (this) {
+      case NodeStatus.running:
+        return Colors.blue;
+      case NodeStatus.success:
+        return Colors.green;
+      case NodeStatus.failure:
+        return Colors.red;
+      case NodeStatus.pending:
+        return Colors.orange;
+      case NodeStatus.inactive:
+        return Colors.grey;
+    }
+  }
 }
