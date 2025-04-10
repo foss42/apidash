691374c86c404d3808f96df8950127abf287c058
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2025-04-10 14:41:09 +0530

    feat: add initial tests for WorkflowCanvas and WorkflowNode rendering (template only)

---
+++ packages/api_testing_suite/lib/src/tests/workflow_builder_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:api_testing_suite/src/workflow_builder/workflow_canvas.dart';
+import 'package:api_testing_suite/src/workflow_builder/workflow_node.dart';
+
+void main() {
+  group('Workflow Builder Tests', () {
+    testWidgets('WorkflowCanvas basic structure', (tester) async {
+      // TODO: Implement basic workflow canvas test
+    });
+
+    testWidgets('WorkflowNode rendering', (tester) async {
+      // TODO: Implement workflow node rendering test
+    });
+  });
+}
