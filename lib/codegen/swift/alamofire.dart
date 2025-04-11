154e3580e590ffe5a4cedbc84b7fe04e52ff2ea3
Nikhil <nikhilljatt@gmail.com>
2025-04-12 02:01:36 +0530

    fixing

--- lib/codegen/swift/alamofire.dart
+++ lib/codegen/swift/alamofire.dart
@@ -4,96 +4,77 @@ import 'package:path/path.dart' as path;
 
 class SwiftAlamofireCodeGen {
   final String kTemplateStart = """
+import Foundation
 import Alamofire
-
-""";
-
-  final String kTemplateFormDataImport = """
-import MultipartFormData
-
 """;
 
   final String kTemplateFormData = '''
-let boundary = try! Boundary()
-let multipartFormData = try! MultipartFormData(boundary: boundary) {
+let multipartFormData = MultipartFormData()
 {% for param in formData %}
     {% if param.type == 'text' %}
-    Subpart {
-        ContentDisposition(name: "{{param.name}}")
-    } body: {
-        Data("{{param.value}}".utf8)
-    }
+multipartFormData.append(Data("{{param.value}}".utf8), withName: "{{param.name}}")
     {% elif param.type == 'file' %}
-    try Subpart {
-        ContentDisposition(name: "{{param.name}}", filename: "{{param.filename}}")
-        ContentType(mimeType: MimeType(pathExtension: "{{param.extension}}"))
-    } body: {
-        try Data(contentsOf: URL(fileURLWithPath: "{{param.filepath}}"))
-    }
+let fileURL = URL(fileURLWithPath: "{{param.filepath}}")
+multipartFormData.append(fileURL, withName: "{{param.name}}", fileName: "{{param.filename}}")
     {% endif %}
 {% endfor %}
-}
-
 ''';
  
   final String kTemplateJsonData = '''
-let parameters = "{{jsonData}}"
-let postData = parameters.data(using: .utf8)
-
+let jsonString = """{{jsonData}}"""
+let jsonData = jsonString.data(using: .utf8)
 ''';
 
   final String kTemplateTextData = '''
-let parameters = "{{textData}}"
-let postData = parameters.data(using: .utf8)
-
+let textString = """{{textData}}"""
+let textData = textString.data(using: .utf8)
 ''';
 
   final String kTemplateRequest = """
-AF.request("{{url}}", method: .{{method}}, parameters: {{parameters}}, encoding: {{encoding}})
-    .responseJSON { response in
-        switch response.result {
-        case .success(let value):
-            print("Response: \\(value)")
-        case .failure(let error):
-            print("Error: \\(error)")
+let url = "{{url}}"
+
+{% if hasFormData %}
+AF.upload(multipartFormData: multipartFormData, to: url, method: .{{method}}{% if hasHeaders %}, headers: {{headers}}{% endif %})
+{% elif hasBody %}
+AF.upload({% if hasJsonData %}jsonData{% else %}textData{% endif %}!, to: url, method: .{{method}}{% if hasHeaders %}, headers: {{headers}}{% endif %})
+{% else %}
+AF.request(url, method: .{{method}}{% if hasHeaders %}, headers: {{headers}}{% endif %})
+{% endif %}
+.responseData { response in
+    switch response.result {
+    case .success(let data):
+        if let responseString = String(data: data, encoding: .utf8) {
+            print("Response: \\(responseString)")
         }
+    case .failure(let error):
+        print("Error: \\(error)")
     }
-""";
+    exit(0)
+}
 
-  final String kTemplateHeaders = """
-{% for header, value in headers %}
-request.addValue("{{value}}", forHTTPHeaderField: "{{header}}")
-{% endfor %}
-
+dispatchMain()
 """;
 
   String? getCode(HttpRequestModel requestModel) {
     try {
       String result = kTemplateStart;
 
-      if (requestModel.hasFormData) {
-        result += kTemplateFormDataImport;
-      }
-
-      var rec =
-          getValidRequestUri(requestModel.url, requestModel.enabledParams);
+      var rec = getValidRequestUri(requestModel.url, requestModel.enabledParams);
       Uri? uri = rec.$1;
-
+      
       var headers = requestModel.enabledHeadersMap;
-      var parameters = 'nil'; 
 
+      bool hasBody = false;
+      bool hasJsonData = false;
       if (requestModel.hasFormData) {
         var formDataList = requestModel.formDataMapList.map((param) {
           if (param['type'] == 'file') {
             final filePath = param['value'] as String;
             final fileName = path.basename(filePath);
-            final fileExtension =
-                path.extension(fileName).toLowerCase().replaceFirst('.', '');
             return {
               'type': 'file',
               'name': param['name'],
               'filename': fileName,
-              'extension': fileExtension,
               'filepath': filePath
             };
           } else {
@@ -109,40 +90,51 @@ request.addValue("{{value}}", forHTTPHeaderField: "{{header}}")
         result += templateFormData.render({
           "formData": formDataList,
         });
-
-        parameters = "multipartFormData"; 
-      } else if (requestModel.hasJsonData) {
+        
+        hasBody = true;
+      } 
+      else if (requestModel.hasJsonData) {
         var templateJsonData = jj.Template(kTemplateJsonData);
         result += templateJsonData.render({
-          "jsonData":
-              requestModel.body!.replaceAll('"', '\\"').replaceAll('\n', '\\n'),
+          "jsonData": requestModel.body!.replaceAll('"', '\\"').replaceAll('\n', '\\n'),
         });
-
-        parameters = "parameters"; 
-      } else if (requestModel.hasTextData) {
+        
+        headers.putIfAbsent("Content-Type", () => "application/json");
+        hasBody = true;
+        hasJsonData = true;
+      } 
+      // Handle text data
+      else if (requestModel.hasTextData) {
         var templateTextData = jj.Template(kTemplateTextData);
         result += templateTextData.render({
-          "textData":
-              requestModel.body!.replaceAll('"', '\\"').replaceAll('\n', '\\n'),
+          "textData": requestModel.body!.replaceAll('"', '\\"').replaceAll('\n', '\\n'),
         });
-
-        parameters = "parameters";
+        
+        headers.putIfAbsent(kHeaderContentType, () => requestModel.bodyContentType.header);
+        hasBody = true;
+      }
+      
+      String headersString = "nil";
+      bool hasHeaders = false;
+      if (headers.isNotEmpty) {
+        List<String> headerItems = [];
+        headers.forEach((key, value) {
+          headerItems.add('"$key": "$value"');
+        });
+        headersString = "[${headerItems.join(', ')}]";
+        hasHeaders = true;
       }
 
       var templateRequest = jj.Template(kTemplateRequest);
       result += templateRequest.render({
         "url": uri.toString(),
         "method": requestModel.method.name.toLowerCase(),
-        "parameters": parameters,
-        "encoding": requestModel.hasFormData ? "JSONEncoding.default" : "URLEncoding.default"
+        "headers": headersString,
+        "hasHeaders": hasHeaders,
+        "hasFormData": requestModel.hasFormData,
+        "hasBody": hasBody,
+        "hasJsonData": hasJsonData
       });
-
-      if (headers.isNotEmpty) {
-        var templateHeader = jj.Template(kTemplateHeaders);
-        result += templateHeader.render({"headers": headers});
-      }
-
-    
 
       return result;
     } catch (e) {
