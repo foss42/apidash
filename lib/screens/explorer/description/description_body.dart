38a90fb8df0d879bb4a1476ec30c9ab39b1d9a9e
Balasubramaniam12007 <balasubramaniam12007@gmail.com>
2025-04-22 19:38:58 +0530

    feat: add ExplorerSplitView for resizable request and description panes

--- lib/screens/explorer/description/description_body.dart
+++ lib/screens/explorer/description/description_body.dart
@@ -3,6 +3,7 @@ import 'package:apidash/models/models.dart';
 import 'package:apidash_core/apidash_core.dart';
 import 'request_pane.dart';
 import 'description_pane.dart';
+import 'explorer_split_view.dart';
 
 class DescriptionBody extends StatefulWidget {
   final ApiTemplate template;
@@ -23,23 +24,16 @@ class _DescriptionBodyState extends State<DescriptionBody> {
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).colorScheme.background,
-      child: Row(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.3,
-            child: RequestsPane(
-              requests: widget.template.requests,
-              onRequestSelected: (request) {
-                setState(() {
-                  _selectedRequest = request;
-                });
-              },
-            ),
-          ),
-          Expanded(
-            child: DescriptionPane(selectedRequest: _selectedRequest),
-          ),
-        ],
+      child: ExplorerSplitView(
+        sidebarWidget: RequestsPane(
+          requests: widget.template.requests,
+          onRequestSelected: (request) {
+            setState(() {
+              _selectedRequest = request;
+            });
+          },
+        ),
+        mainWidget: DescriptionPane(selectedRequest: _selectedRequest),
       ),
     );
   }
