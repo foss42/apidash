ac5c1da9f8e23ee39c49832f16805e056f719c8b
Nikhil <nikhilljatt@gmail.com>
2025-05-18 18:39:26 +0530

    updated C curl tests

--- test/codegen/c_curl_codegen_test.dart
+++ test/codegen/c_curl_codegen_test.dart
@@ -147,7 +147,7 @@ int main() {
   CURLcode res;
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.apidash.dev/country/data?code=IND");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.apidash.dev/country/data?code=US&code=IND");
     struct ResponseData response_data = {0};
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
