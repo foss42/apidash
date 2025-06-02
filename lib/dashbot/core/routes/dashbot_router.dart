2cfa67cb53523340249744820d21000538a1a5cf
Udhay-Adithya <udhayxd@gmail.com>
2025-06-02 22:17:22 +0530

    feat: add DashbotHomePage and update routing for home page

--- lib/dashbot/core/routes/dashbot_router.dart
+++ lib/dashbot/core/routes/dashbot_router.dart
@@ -1,15 +1,16 @@
 import 'package:apidash/dashbot/core/routes/dashbot_routes.dart';
 import 'package:apidash/dashbot/core/common/pages/dashbot_default_page.dart';
+import 'package:apidash/dashbot/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic>? generateRoute(
   RouteSettings settings,
 ) {
   switch (settings.name) {
-    // case (DashbotRoutes.dashbotHome):
-    //   return MaterialPageRoute(
-    //     builder: (context) => DashbotHomePage(),
-    //   );
+    case (DashbotRoutes.dashbotHome):
+      return MaterialPageRoute(
+        builder: (context) => DashbotHomePage(),
+      );
     case (DashbotRoutes.dashbotDefault):
       return MaterialPageRoute(
         builder: (context) => DashbotDefaultPage(),
