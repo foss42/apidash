7d2c29cc0d053bc23326d36d4e7a25bbbca8d548
Ashita Prasad <ashitaprasad92@gmail.com>
2025-06-22 21:23:55 +0530

    Update kJSSetupScript & add js_utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,6 +4,7 @@ export 'file_utils.dart';
 export 'har_utils.dart';
 export 'header_utils.dart';
 export 'history_utils.dart';
+export 'js_utils.dart';
 export 'http_utils.dart';
 export 'save_utils.dart';
 export 'ui_utils.dart';
