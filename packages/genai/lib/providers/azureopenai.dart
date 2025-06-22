69e388235779075ff004244032461c150fff5715
Manas Hejmadi <manas2all@outlook.com>
2025-06-22 23:07:06 +0530

    Providers: Implemented anthropic, gemini, openai & azureopenai

---
+++ packages/genai/lib/providers/azureopenai.dart
@@ -0,0 +1,77 @@
+import '../llm_config.dart';
+import '../llm_input_payload.dart';
+import '../llm_request.dart';
+import 'common.dart';
+
+class AzureOpenAIModelController extends ModelController {
+  static final instance = AzureOpenAIModelController();
+  @override
+  LLMInputPayload get inputPayload => LLMInputPayload(
+    endpoint: '', //TO BE FILLED BY USER
+    credential: '',
+    systemPrompt: '',
+    userPrompt: '',
+    configMap: {
+      LLMConfigName.temperature.name:
+          defaultLLMConfigurations[LLMConfigName.temperature]!,
+      LLMConfigName.top_p.name: defaultLLMConfigurations[LLMConfigName.top_p]!,
+    },
+  ).clone();
+
+  @override
+  LLMRequestDetails createRequest(
+    LLMModel model,
+    LLMInputPayload inputPayload, {
+    bool stream = false,
+  }) {
+    if (inputPayload.endpoint.isEmpty) {
+      throw Exception('MODEL ENDPOINT IS EMPTY');
+    }
+    return LLMRequestDetails(
+      endpoint: inputPayload.endpoint,
+      headers: {'api-key': inputPayload.credential},
+      method: 'POST',
+      body: {
+        if (stream) ...{'stream': true},
+        "messages": [
+          {"role": "system", "content": inputPayload.systemPrompt},
+          if (inputPayload.userPrompt.isNotEmpty) ...{
+            {"role": "user", "content": inputPayload.userPrompt},
+          } else ...{
+            {"role": "user", "content": "Generate"},
+          },
+        ],
+        "temperature":
+            inputPayload
+                .configMap[LLMConfigName.temperature.name]
+                ?.configValue
+                .value
+                ?.$2 ??
+            0.5,
+        "top_p":
+            inputPayload
+                .configMap[LLMConfigName.top_p.name]
+                ?.configValue
+                .value
+                ?.$2 ??
+            0.95,
+        if (inputPayload.configMap[LLMConfigName.max_tokens.name] != null) ...{
+          "max_tokens": inputPayload
+              .configMap[LLMConfigName.max_tokens.name]!
+              .configValue
+              .value,
+        },
+      },
+    );
+  }
+
+  @override
+  String? outputFormatter(Map x) {
+    return x["choices"]?[0]["message"]?["content"]?.trim();
+  }
+
+  @override
+  String? streamOutputFormatter(Map x) {
+    return x["choices"]?[0]["delta"]?["content"];
+  }
+}
---
+++ packages/genai/lib/providers/gemini.dart
@@ -0,0 +1,92 @@
+import '../llm_config.dart';
+import '../llm_input_payload.dart';
+import '../llm_request.dart';
+import 'common.dart';
+
+class GeminiModelController extends ModelController {
+  static final instance = GeminiModelController();
+  @override
+  LLMInputPayload get inputPayload => LLMInputPayload(
+    endpoint: 'https://generativelanguage.googleapis.com/v1beta/models',
+    credential: '',
+    systemPrompt: '',
+    userPrompt: '',
+    configMap: {
+      LLMConfigName.temperature.name:
+          defaultLLMConfigurations[LLMConfigName.temperature]!,
+      LLMConfigName.top_p.name: defaultLLMConfigurations[LLMConfigName.top_p]!,
+    },
+  ).clone();
+
+  @override
+  LLMRequestDetails createRequest(
+    LLMModel model,
+    LLMInputPayload inputPayload, {
+    bool stream = false,
+  }) {
+    String endpoint = inputPayload.endpoint;
+    endpoint =
+        "$endpoint/${model.identifier}:generateContent?key=${inputPayload.credential}";
+    if (stream) {
+      endpoint = endpoint.replaceAll(
+        'generateContent?',
+        'streamGenerateContent?alt=sse&',
+      );
+    }
+    return LLMRequestDetails(
+      endpoint: endpoint,
+      headers: {},
+      method: 'POST',
+      body: {
+        "model": model.identifier,
+        "contents": [
+          {
+            "role": "user",
+            "parts": [
+              {"text": inputPayload.userPrompt},
+            ],
+          },
+        ],
+        "systemInstruction": {
+          "role": "system",
+          "parts": [
+            {"text": inputPayload.systemPrompt},
+          ],
+        },
+        "generationConfig": {
+          "temperature":
+              inputPayload
+                  .configMap[LLMConfigName.temperature.name]
+                  ?.configValue
+                  .value
+                  ?.$2 ??
+              0.5,
+          "topP":
+              inputPayload
+                  .configMap[LLMConfigName.top_p.name]
+                  ?.configValue
+                  .value
+                  ?.$2 ??
+              0.95,
+          if (inputPayload.configMap[LLMConfigName.max_tokens.name] !=
+              null) ...{
+            "maxOutputTokens": inputPayload
+                .configMap[LLMConfigName.max_tokens.name]!
+                .configValue
+                .value,
+          },
+        },
+      },
+    );
+  }
+
+  @override
+  String? outputFormatter(Map x) {
+    return x['candidates']?[0]?['content']?['parts']?[0]?['text'];
+  }
+
+  @override
+  String? streamOutputFormatter(Map x) {
+    return x['candidates']?[0]?['content']?['parts']?[0]?['text'];
+  }
+}
