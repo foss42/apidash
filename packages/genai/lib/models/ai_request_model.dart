ba8890c40c2af4c54bfc1764872f2e8b4e974ac2
Manas Hejmadi <manas2all@outlook.com>
2025-06-22 23:12:40 +0530

    AIRequestModel & AIResponseModel Created

---
+++ packages/genai/lib/models/ai_request_model.dart
@@ -0,0 +1,61 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../llm_saveobject.dart';
+import '../llm_input_payload.dart';
+import '../llm_request.dart';
+import '../providers/common.dart';
+import '../providers/gemini.dart';
+import '../providers/providers.dart';
+part 'ai_request_model.freezed.dart';
+part 'ai_request_model.g.dart';
+
+@freezed
+class AIRequestModel with _$AIRequestModel {
+  const AIRequestModel._();
+
+  @JsonSerializable(explicitToJson: true, anyMap: true)
+  factory AIRequestModel({
+    @JsonKey(fromJson: LLMInputPayload.fromJSON, toJson: LLMInputPayload.toJSON)
+    required LLMInputPayload payload,
+    @JsonKey(fromJson: LLMModel.fromJson, toJson: LLMModel.toJson)
+    required LLMModel model,
+    @JsonKey(fromJson: LLMProvider.fromJSON, toJson: LLMProvider.toJSON)
+    required LLMProvider provider,
+  }) = _AIRequestModel;
+
+  factory AIRequestModel.fromJson(Map<String, Object?> json) =>
+      _$AIRequestModelFromJson(json);
+
+  AIRequestModel updatePayload(LLMInputPayload p) {
+    return AIRequestModel(payload: p, model: model, provider: provider);
+  }
+
+  LLMRequestDetails createRequest() {
+    final controller = model.provider.modelController;
+    return controller.createRequest(model, payload);
+  }
+
+  factory AIRequestModel.fromDefaultSaveObject(LLMSaveObject? defaultLLMSO) {
+    final gmC = GeminiModelController.instance;
+    return AIRequestModel(
+      model:
+          defaultLLMSO?.selectedLLM ??
+          LLMProvider.gemini.getLLMByIdentifier('gemini-2.0-flash'),
+      provider: defaultLLMSO?.provider ?? LLMProvider.gemini,
+      payload: LLMInputPayload(
+        endpoint: defaultLLMSO?.endpoint ?? gmC.inputPayload.endpoint,
+        credential: defaultLLMSO?.credential ?? '',
+        systemPrompt: '',
+        userPrompt: '',
+        configMap: defaultLLMSO?.configMap ?? gmC.inputPayload.configMap,
+      ),
+    );
+  }
+
+  AIRequestModel clone() {
+    return AIRequestModel(
+      model: model,
+      payload: payload.clone(),
+      provider: provider,
+    );
+  }
+}
---
+++ packages/genai/lib/models/ai_request_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ai_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AIRequestModelImpl _$$AIRequestModelImplFromJson(Map json) =>
+    _$AIRequestModelImpl(
+      payload: LLMInputPayload.fromJSON(json['payload'] as Map),
+      model: LLMModel.fromJson(json['model'] as Map),
+      provider: LLMProvider.fromJSON(json['provider'] as Map),
+    );
+
+Map<String, dynamic> _$$AIRequestModelImplToJson(
+  _$AIRequestModelImpl instance,
+) => <String, dynamic>{
+  'payload': LLMInputPayload.toJSON(instance.payload),
+  'model': LLMModel.toJson(instance.model),
+  'provider': LLMProvider.toJSON(instance.provider),
+};
