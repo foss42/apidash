5afce7438b36fdea4a19e5e1743125ebcfd56b71
Ankit Mahato <ankmahato@gmail.com>
2025-07-12 15:52:47 +0530

    Update consts.dart

--- lib/screens/common_widgets/auth/consts.dart
+++ lib/screens/common_widgets/auth/consts.dart
@@ -7,3 +7,6 @@ final kAddToDefaultLocation = kAddToLocations[0].$1;
 final kAddToLocationsMap = {for (var v in kAddToLocations) v.$1: v.$2};
 const kLabelAddTo = "Add to";
 const kTooltipApiKeyAuth = "Select where to add API key";
+const kHintTextFieldName = "Header/Query Param Name";
+const kLabelApiKey = "API Key";
+const kHintTextKey = "Key";
