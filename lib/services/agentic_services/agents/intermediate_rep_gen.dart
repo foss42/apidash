9bb51ba4506509a8495ea96359c824bc2a7e8ab8
Manas Hejmadi <manas2all@outlook.com>
2025-08-03 17:38:13 +0530

    ADDED: AgenticServices & AI_UI_DESIGNER Agents

---
+++ lib/services/agentic_services/agents/intermediate_rep_gen.dart
@@ -0,0 +1,67 @@
+import 'package:genai/agentic_engine/blueprint.dart';
+
+const String kIntermediateRepGenSystemPrompt = """
+You are an expert at converting API Responses into a YAML schema tree.
+When you get a given JSON API Response, I want you to break it down and recombine it in the form of a YAMK UI schema.
+
+Sample Schema:
+```yaml
+- type: column
+  elements:
+    - type: row
+      elements:
+        - type: image
+          src: "https://reqres.in/img/faces/7-image.jpg"
+          shape: circle
+          width: 60
+          height: 60
+        - type: column
+          elements:
+            - type: text
+              data: "Michael Lawson"
+              font: "segoe-ui"
+              color: blue
+            - type: text
+              data: "michael.lawson@reqres.in"
+              font: "segoe-ui"
+              color: gray
+```
+
+API_RESPONSE: ```json
+:VAR_API_RESPONSE:
+```
+
+USE the API_RESPONSE to generate this YAML representation.
+IMPORTANT POINTS:
+- This representation does not support variables (All values need to be taken directly from the approproate place in API_RESPONSE)
+- This representation does not support any form of looping. Hence if there are 5 repeated elements with different data, repeat the representation and use actual data from API_RESPONSE
+
+DO NOT START OR END THE RESPONSE WITH ANYTHING ELSE. I WANT PURE YAML OUTPUT
+  """;
+
+class IntermediateRepresentationGen extends APIDashAIAgent {
+  @override
+  String get agentName => 'INTERMEDIATE_REP_GEN';
+
+  @override
+  String getSystemPrompt() {
+    return kIntermediateRepGenSystemPrompt;
+  }
+
+  @override
+  Future<bool> validator(String aiResponse) async {
+    //Add any specific validations here as needed
+    return true;
+  }
+
+  @override
+  Future outputFormatter(String validatedResponse) async {
+    validatedResponse = validatedResponse
+        .replaceAll('```yaml', '')
+        .replaceAll('```yaml\n', '')
+        .replaceAll('```', '');
+    return {
+      'INTERMEDIATE_REPRESENTATION': validatedResponse,
+    };
+  }
+}
---
+++ lib/services/agentic_services/agents/semantic_analyser.dart
@@ -0,0 +1,38 @@
+import 'package:genai/agentic_engine/blueprint.dart';
+
+const String kSemanticAnalyserSystemPrompt = """
+You are an expert at understanding API response structures. 
+When i provide a sample JSON response, Please give me a semantic analysis about it.
+
+AVOID USAGE OF MARKDOWN. I WANT SIMPLE PLAIN_TEXT OUTPUT.
+Keep in mind that the content you generate will be used as input to another AI Bot so please be crisp and to the point.
+
+KEEP only fields that make sense to the UI and instruct to omit other things
+
+THe next bot will be a UI Generator bot that will take your semantic analysis and use it to generate UI, so tailor the details accordingly.
+I do not want any format, Just a large paragraph of plaintext explaining what the response is and what type of UI will be best suited
+  DO NOT START OR END THE RESPONSE WITH ANYTHING ELSE. I WANT PURE OUTPUT
+  """;
+
+class ResponseSemanticAnalyser extends APIDashAIAgent {
+  @override
+  String get agentName => 'RESP_SEMANTIC_ANALYSER';
+
+  @override
+  String getSystemPrompt() {
+    return kSemanticAnalyserSystemPrompt;
+  }
+
+  @override
+  Future<bool> validator(String aiResponse) async {
+    //Add any specific validations here as needed
+    return true;
+  }
+
+  @override
+  Future outputFormatter(String validatedResponse) async {
+    return {
+      'SEMANTIC_ANALYSIS': validatedResponse,
+    };
+  }
+}
