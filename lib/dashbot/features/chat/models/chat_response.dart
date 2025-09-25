066aa0fcf4ce553b1c2b3f94e9d25b6dac372ec7
Udhay-Adithya <udhayxd@gmail.com>
2025-09-25 13:37:53 +0530

    tests: add chat model tests(cv: 100)

--- lib/dashbot/features/chat/models/chat_response.dart
+++ lib/dashbot/features/chat/models/chat_response.dart
@@ -18,18 +18,4 @@ class ChatResponse {
       messageType: messageType ?? this.messageType,
     );
   }
-
-  @override
-  String toString() =>
-      'ChatResponse(content: $content, messageType: $messageType)';
-
-  @override
-  bool operator ==(covariant ChatResponse other) {
-    if (identical(this, other)) return true;
-
-    return other.content == content && other.messageType == messageType;
-  }
-
-  @override
-  int get hashCode => content.hashCode ^ messageType.hashCode;
 }
