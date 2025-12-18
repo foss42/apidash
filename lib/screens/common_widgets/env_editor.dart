6487e15e8af93743f15966c5ec9525edd4e54b0f
Mohamed Adel Kamal <raynowing007@gmail.com>
2025-12-18 08:28:16 +0200

    feat: env variable support for text request body

---
+++ lib/screens/common_widgets/env_editor.dart
@@ -0,0 +1,112 @@
+﻿import 'dart:math' as math;
+import 'package:apidash/consts.dart';
+import 'package:apidash/screens/common_widgets/common_widgets.dart';
+import 'package:apidash_design_system/apidash_design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_trigger_autocomplete_plus/multi_trigger_autocomplete_plus.dart';
+
+
+class EnvironmentTextFieldEditor extends StatefulWidget{
+  const EnvironmentTextFieldEditor({
+    super.key,
+    required this.fieldKey,
+    this.onChanged,
+    this.initialValue,
+    this.readOnly = false,
+    this.hintText,
+  });
+
+  final String fieldKey;
+  final Function(String)? onChanged;
+  final String? initialValue;
+  final bool readOnly;
+  final String? hintText;
+
+  @override
+  State<StatefulWidget> createState() => _EnvironmentTextField();
+}
+
+
+class _EnvironmentTextField extends State<EnvironmentTextFieldEditor>{
+  final TextEditingController controller = TextEditingController();
+  late final FocusNode editorFocusNode;
+
+
+  void insertTab() {
+    String sp = "  ";
+    int offset = math.min(
+        controller.selection.baseOffset, controller.selection.extentOffset);
+    String text = controller.text.substring(0, offset) +
+        sp +
+        controller.text.substring(offset);
+    controller.value = TextEditingValue(
+      text: text,
+      selection: controller.selection.copyWith(
+        baseOffset: controller.selection.baseOffset + sp.length,
+        extentOffset: controller.selection.extentOffset + sp.length,
+      ),
+    );
+    widget.onChanged?.call(text);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    editorFocusNode = FocusNode(debugLabel: "Editor Focus Node");
+  }
+
+  @override
+  void dispose() {
+    editorFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CallbackShortcuts(
+      bindings: <ShortcutActivator, VoidCallback>{
+        const SingleActivator(LogicalKeyboardKey.tab): () {
+          insertTab();
+        },
+      },
+      child: EnvironmentTriggerField(
+        keyId: widget.fieldKey,
+        controller: controller,
+        focusNode: editorFocusNode,
+        keyboardType: TextInputType.multiline,
+        expands: true,
+        maxLines: null,
+        readOnly: widget.readOnly,
+        style: kCodeStyle.copyWith(
+          fontSize: Theme.of(context).textTheme.bodyMedium?.fontSize,
+        ),
+        textAlignVertical: TextAlignVertical.top,
+        onChanged: widget.onChanged,
+        decoration: InputDecoration(
+          hintText: widget.hintText ?? kHintContent,
+          hintStyle: TextStyle(
+            color: Theme.of(context).colorScheme.outlineVariant,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: kBorderRadius8,
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.outlineVariant,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: kBorderRadius8,
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.surfaceContainerHighest,
+            ),
+          ),
+          filled: true,
+          hoverColor: kColorTransparent,
+          fillColor: Theme.of(context).colorScheme.surfaceContainerLowest,
+        ),
+        optionsAlignment: OptionsAlignment.topEnd,
+      ),
+    );
+  }
+
+}
