4a2907cd0dde26c1cd8cb3a9b896b734467a5979
ShashwatXD <legendshashwat.gkp@gmail.com>
2026-01-22 22:24:56 +0530

    made proxy settings model

--- packages/better_networking/lib/models/proxy_settings_model.dart
+++ packages/better_networking/lib/models/proxy_settings_model.dart
@@ -1,69 +1,17 @@
-import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-@immutable
-class ProxySettings {
-  const ProxySettings({
-    required this.host,
-    required this.port,
-    this.username,
-    this.password,
-  });
+part 'proxy_settings_model.freezed.dart';
+part 'proxy_settings_model.g.dart';
 
-  final String host;
-  final String port;
-  final String? username;
-  final String? password;
-
-  ProxySettings copyWith({
-    String? host,
-    String? port,
+@freezed
+abstract class ProxySettings with _$ProxySettings {
+  const factory ProxySettings({
+    required String host,
+    required String port,
     String? username,
     String? password,
-  }) {
-    return ProxySettings(
-      host: host ?? this.host,
-      port: port ?? this.port,
-      username: username ?? this.username,
-      password: password ?? this.password,
-    );
-  }
+  }) = _ProxySettings;
 
-  factory ProxySettings.fromJson(Map<String, dynamic> json) {
-    return ProxySettings(
-      host: json["host"] as String,
-      port: json["port"] as String,
-      username: json["username"] as String?,
-      password: json["password"] as String?,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "host": host,
-      "port": port,
-      "username": username,
-      "password": password,
-    };
-  }
-
-  @override
-  String toString() {
-    return 'ProxySettings(host: $host, port: $port, username: $username, password: $password)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is ProxySettings &&
-        other.host == host &&
-        other.port == port &&
-        other.username == username &&
-        other.password == password;
-  }
-
-  @override
-  int get hashCode {
-    return Object.hash(host, port, username, password);
-  }
+  factory ProxySettings.fromJson(Map<String, dynamic> json) =>
+      _$ProxySettingsFromJson(json);
 }
