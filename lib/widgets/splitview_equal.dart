03da3ea9d5290fba3dbecf11a18159b28cee07d6
kpradyun <kpradyun@hotmail.com>
2026-02-21 17:03:05 +0530

    Fix issue #1090: Request/Response pane widths reset when toggling View Code

--- lib/widgets/splitview_equal.dart
+++ lib/widgets/splitview_equal.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:multi_split_view/multi_split_view.dart';
 
-class EqualSplitView extends StatelessWidget {
+class EqualSplitView extends StatefulWidget {
   const EqualSplitView({
     super.key,
     required this.leftWidget,
@@ -11,7 +11,32 @@ class EqualSplitView extends StatelessWidget {
   final Widget leftWidget;
   final Widget rightWidget;
 
-  getMinFractionWidth(double width) {
+  @override
+  State<EqualSplitView> createState() => _EqualSplitViewState();
+}
+
+class _EqualSplitViewState extends State<EqualSplitView> {
+  late MultiSplitViewController _controller;
+  double? _lastMinWidth;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = MultiSplitViewController(
+      areas: [
+        Area(id: "left", flex: 1),
+        Area(id: "right", flex: 1),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  double getMinFractionWidth(double width) {
     if (width < 900) {
       return 0.9;
     } else if (width < 1000) {
@@ -38,18 +63,24 @@ class EqualSplitView extends StatelessWidget {
       child: LayoutBuilder(
         builder: (context, constraints) {
           final minWidth = getMinFractionWidth(constraints.maxWidth);
+          if (_lastMinWidth != minWidth) {
+            _lastMinWidth = minWidth;
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              if (mounted) {
+                for (var area in _controller.areas) {
+                  area.min = minWidth;
+                }
+              }
+            });
+          }
+
           return MultiSplitView(
-            controller: MultiSplitViewController(
-              areas: [
-                Area(id: "left", flex: 1, min: minWidth),
-                Area(id: "right", flex: 1, min: minWidth),
-              ],
-            ),
+            controller: _controller,
             builder: (context, area) {
               return switch (area.id) {
-                "left" => leftWidget,
-                "right" => rightWidget,
-                _ => Container(),
+                "left" => widget.leftWidget,
+                "right" => widget.rightWidget,
+                _ => const SizedBox.shrink(),
               };
             },
           );
