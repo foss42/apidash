67c9445242f75ad431905f2dcdfc69116be6fdf4
Ankit Mahato <ankmahato@gmail.com>
2026-03-08 05:07:57 +0530

    Update response_model_test.dart

--- test/models/response_model_test.dart
+++ test/models/response_model_test.dart
@@ -24,8 +24,10 @@ void main() {
 
     final responseData = responseModel.fromResponse(response: responseRec.$1!);
     expect(responseData.statusCode, 200);
-    expect(responseData.body,
-        '{"data":"Check out https://api.apidash.dev/docs to get started."}');
+    expect(
+      responseData.body,
+      '{"data":"Check out https://api.apidash.dev/docs to get started."}',
+    );
     expect(responseData.formattedBody, '''{
   "data": "Check out https://api.apidash.dev/docs to get started."
 }''');
@@ -47,33 +49,41 @@ void main() {
     expect(responseData.mediaType!.mimeType, 'text/html');
   });
 
-  test('Testing contentType override by the user having no charset (#630)',
-      () async {
-    var responseRec = await sendHttpRequest(
-      requestModelPost11.id,
-      requestModelPost11.apiType,
-      requestModelPost11.httpRequestModel!,
-    );
+  test(
+    'Testing contentType override by the user having no charset (#630)',
+    () async {
+      var responseRec = await sendHttpRequest(
+        requestModelPost11.id,
+        requestModelPost11.apiType,
+        requestModelPost11.httpRequestModel!,
+      );
 
-    final responseData = responseModel.fromResponse(response: responseRec.$1!);
-    expect(responseData.statusCode, 200);
-    expect(responseData.body, '{"data":"i love flutter"}');
-    expect(responseData.contentType, 'application/json');
-    expect(responseData.requestHeaders?['content-type'], 'application/json');
-  });
+      final responseData = responseModel.fromResponse(
+        response: responseRec.$1!,
+      );
+      expect(responseData.statusCode, 200);
+      expect(responseData.body, '{"data":"i love flutter"}');
+      expect(responseData.contentType, 'application/json');
+      expect(responseData.requestHeaders?['content-type'], 'application/json');
+    },
+  );
 
-  test('Testing default contentType charset added by dart', () async {
-    var responseRec = await sendHttpRequest(
-      requestModelPost12.id,
-      requestModelPost12.apiType,
-      requestModelPost12.httpRequestModel!,
-    );
+  test(
+    'Testing default contentType charset no longer added by dart http v1.6.0',
+    () async {
+      var responseRec = await sendHttpRequest(
+        requestModelPost12.id,
+        requestModelPost12.apiType,
+        requestModelPost12.httpRequestModel!,
+      );
 
-    final responseData = responseModel.fromResponse(response: responseRec.$1!);
-    expect(responseData.statusCode, 200);
-    expect(responseData.requestHeaders?['content-type'],
-        'application/json; charset=utf-8');
-  });
+      final responseData = responseModel.fromResponse(
+        response: responseRec.$1!,
+      );
+      expect(responseData.statusCode, 200);
+      expect(responseData.requestHeaders?['content-type'], 'application/json');
+    },
+  );
 
   test('Testing latin1 charset added by user', () async {
     var responseRec = await sendHttpRequest(
@@ -84,8 +94,10 @@ void main() {
 
     final responseData = responseModel.fromResponse(response: responseRec.$1!);
     expect(responseData.statusCode, 200);
-    expect(responseData.requestHeaders?['content-type'],
-        'application/json; charset=latin1');
+    expect(
+      responseData.requestHeaders?['content-type'],
+      'application/json; charset=latin1',
+    );
   });
 
   test('Testing fromResponse for Bad SSL with certificate check', () async {
@@ -131,14 +143,20 @@ void main() {
 
     final responseData = responseModel.fromResponse(response: responseRec.$1!);
     expect(responseData.statusCode, 200);
-    expect(responseData.headers?['access-control-allow-methods'],
-        'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS');
     expect(
-        responseData.headers?['access-control-allow-methods']
-            ?.contains("OPTIONS"),
-        true);
-    expect(responseData.headers?['allow'],
-        'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS');
+      responseData.headers?['access-control-allow-methods'],
+      'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
+    );
+    expect(
+      responseData.headers?['access-control-allow-methods']?.contains(
+        "OPTIONS",
+      ),
+      true,
+    );
+    expect(
+      responseData.headers?['allow'],
+      'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
+    );
     expect(responseData.headers?['allow']?.contains("OPTIONS"), true);
   });
 }
