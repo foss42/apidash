810161afa0b6cc0bffc546b0a3385ca847ed4f12
tejaswinisa1 <tejaswinisa39@gmail.com>
2026-03-30 16:15:27 +0530

    fix: remove keyboard inset padding from mobile bottom bar

--- lib/screens/mobile/requests_page/request_response_page_bottombar.dart
+++ lib/screens/mobile/requests_page/request_response_page_bottombar.dart
@@ -14,59 +14,56 @@ class RequestResponsePageBottombar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: MediaQuery.of(context).viewInsets,
-      child: Container(
-        height: 60 + MediaQuery.paddingOf(context).bottom,
-        width: MediaQuery.sizeOf(context).width,
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.paddingOf(context).bottom,
-          left: 16,
-          right: 16,
-        ),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surface,
-          border: Border(
-            top: BorderSide(
-              color: Theme.of(context).colorScheme.onInverseSurface,
-              width: 1,
-            ),
+    return Container(
+      height: 60 + MediaQuery.paddingOf(context).bottom,
+      width: MediaQuery.sizeOf(context).width,
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.paddingOf(context).bottom,
+        left: 16,
+        right: 16,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        border: Border(
+          top: BorderSide(
+            color: Theme.of(context).colorScheme.onInverseSurface,
+            width: 1,
           ),
         ),
-        child: Row(
-          children: [
-            ADFilledButton(
-              onPressed: () {
-                ref
-                    .read(dashbotShowMobileProvider.notifier)
-                    .update((state) => !state);
-              },
-              isTonal: true,
-              items: [
-                kHSpacer6,
-                Text(
-                  ref.watch(dashbotShowMobileProvider)
-                      ? kLabelClose
-                      : kLabelDashBot,
-                  style: kTextStyleButton,
-                ),
-                kHSpacer6,
-              ],
-            ),
-            const Spacer(),
-            SizedBox(
-              height: 36,
-              child: SendRequestButton(
-                onTap: () {
-                  ref.read(dashbotShowMobileProvider.notifier).state = false;
-                  if (requestTabController.index != 1) {
-                    requestTabController.animateTo(1);
-                  }
-                },
+      ),
+      child: Row(
+        children: [
+          ADFilledButton(
+            onPressed: () {
+              ref
+                  .read(dashbotShowMobileProvider.notifier)
+                  .update((state) => !state);
+            },
+            isTonal: true,
+            items: [
+              kHSpacer6,
+              Text(
+                ref.watch(dashbotShowMobileProvider)
+                    ? kLabelClose
+                    : kLabelDashBot,
+                style: kTextStyleButton,
               ),
+              kHSpacer6,
+            ],
+          ),
+          const Spacer(),
+          SizedBox(
+            height: 36,
+            child: SendRequestButton(
+              onTap: () {
+                ref.read(dashbotShowMobileProvider.notifier).state = false;
+                if (requestTabController.index != 1) {
+                  requestTabController.animateTo(1);
+                }
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
