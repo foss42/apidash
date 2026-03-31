3c2f037baecada8e3c233310ae810c7d6ec02b72
Tanmay Agarwal <agarwal.tanmay.work@gmail.com>
2026-03-31 15:27:21 +0530

    fix(genai): replace debugPrint with dart:developer log() to reduce Flutter coupling

--- packages/genai/lib/utils/model_manager.dart
+++ packages/genai/lib/utils/model_manager.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:better_networking/better_networking.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:developer';
 import '../consts.dart';
 import '../interface/interface.dart';
 import '../models/models.dart';
@@ -19,13 +19,13 @@ class ModelManager {
         ),
       );
       if (resp == null) {
-        debugPrint('fetchModelsFromRemote -> resp == null');
+        log('fetchModelsFromRemote -> resp == null');
       } else {
         var remoteModels = availableModelsFromJson(resp.body);
         return remoteModels;
       }
     } catch (e) {
-      debugPrint('fetchModelsFromRemote -> ${e.toString()}');
+      log('fetchModelsFromRemote -> ${e.toString()}');
     }
     return null;
   }
@@ -57,7 +57,7 @@ class ModelManager {
         );
       }
     } catch (e) {
-      debugPrint('fetchAvailableModels -> ${e.toString()}');
+      log('fetchAvailableModels -> ${e.toString()}');
     }
     return kAvailableModels;
   }
@@ -77,7 +77,7 @@ class ModelManager {
         HttpRequestModel(url: url, method: HTTPVerb.get),
         noSSL: true,
       );
-      // debugPrint("fetchInstalledOllamaModels -> $url -> ${resp?.body} -> $msg");
+      // log("fetchInstalledOllamaModels -> $url -> ${resp?.body} -> $msg");
       if (resp == null) {
         return null;
       }
@@ -90,7 +90,7 @@ class ModelManager {
       }
       return ollamaModels;
     } catch (e) {
-      debugPrint('fetchInstalledOllamaModels -> ${e.toString()}');
+      log('fetchInstalledOllamaModels -> ${e.toString()}');
       return null;
     }
   }
